ed705f89edcdca931a23ada68f5046145a2d459b
Dan Melton <melton.dan@gmail.com>
2019-03-25 20:47:09 -0700

    gets user model setup

--- api/models/User.ts
+++ api/models/User.ts
@@ -1,67 +1,45 @@
-import * as bcrypt from 'bcryptjs';
-import {
-    BelongsTo,
-    BelongsToCreateAssociationMixin,
-    BelongsToGetAssociationMixin,
-    BelongsToSetAssociationMixin,
+import Sequelize, {
     DataTypes,
-    FindOptions,
-    Model,
+    Model, ModelAttributes,
 } from 'sequelize';
-// import sequelize from './db';
-
+import { db } from './db'
 
 class User extends Model {
-    static init(sequelize) {
-        return super.init({
-            username: DataTypes.STRING,
-            firstName: DataTypes.STRING,
-            lastName: DataTypes.STRING,
-            email: DataTypes.STRING,
-            password: DataTypes.STRING,
-        }, { sequelize });
-    }
-
-    public static associations: {
-        group: BelongsTo
-    };
-    validPassword(password: string) {
-        return bcrypt.compareSync(password, this.password);
-    }
-
     public id: number;
     public email: string;
     public password: string;
     public username: string;
     public firstName: string;
     public lastName: string;
+    public woot: string;
     public createdAt: Date;
     public updatedAt: Date;
-
-    // mixins for association (optional)
-    // public groupId: number;
-    // public group: UserGroup;
-    // public getGroup: BelongsToGetAssociationMixin<UserGroup>;
-    // public setGroup: BelongsToSetAssociationMixin<UserGroup, number>;
-    // public createGroup: BelongsToCreateAssociationMixin<UserGroup>;
-
 }
-// Hooks
-// User.beforeCreate((user) => {
-//     const salt = bcrypt.genSaltSync();
-//     user.password = bcrypt.hashSync(user.password, salt);
-// });
-// User.afterFind((users, options: FindOptions) => {
-//     console.log('found');
-// });
-// User.prototype.validPassword = (password) => {
-//     return bcrypt.compareSync(password, this.password);
-// };
+
+User.init({
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    woot: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    tableName: 'Users',
+    sequelize: db
+});
 
 export default User;
-
-
-// associate
-// it is important to import _after_ the model above is already exported so the circular reference works.
-// import { UserGroup } from './UserGroup';
-// export const Group = User.belongsTo(UserGroup, { as: 'group', foreignKey: 'groupId' });
--- api/models/db.ts
+++ api/models/db.ts
@@ -1,30 +1,4 @@
-import sequelizeConfig from '../config';
+import { dbConfig } from '../config/db';
 import * as Sequelize from 'sequelize';
-const env = process.env.NODE_ENV;
-import User from './user';
 
-const sequelize = new Sequelize.Sequelize(sequelizeConfig[env]);
-
-// User.init(sequelize);
-// const User = sequelize.import('./user')
-User.init(sequelize)
-const models = {
-  User: User,
-  sequelize,
-  Sequelize,
-  op: Sequelize.Op
-};
-
-Object.keys(models).forEach((modelName) => {
-  if ('associate' in models[modelName]) {
-    models[modelName].associate(models);
-  }
-});
-
-// models.sequelize = sequelize;
-// models.Sequelize = Sequelize;
-// models.op = Sequelize.Op;
-
-sequelize.sync().then(() => console.log('synced with db'));
-
-export default models;
+export const db = new Sequelize.Sequelize(dbConfig);
