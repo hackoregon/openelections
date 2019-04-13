f00d65969c9131082aba9539f0a5a02cddb7a205
Andrew <andyerickson2014@gmail.com>
2019-04-12 20:15:43 -0700

    WIP: begin incorporating configureStore into project for storybook purposes GH-70

---
+++ app/stories/Provider.tsx
@@ -0,0 +1,13 @@
+// import * as React from 'react';
+// import { Provider as ReduxProvider } from 'react-redux';
+// import configureStore from '../store/configureStore';
+// ​
+// const store = configureStore();
+// ​
+// export default function Provider({ story }) {
+//   return (
+//     <ReduxProvider store={store}>
+//       {story}
+//     </ReduxProvider>
+//   );
+// };
--- app/stories/index.stories.tsx
+++ app/stories/index.stories.tsx
@@ -1,29 +1,14 @@
 import * as React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+// import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Welcome } from '@storybook/react/demo';
+import Doohicky from '../src/components/Doohicky/index';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
 
-  storiesOf('Button1', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+storiesOf('Doohicky', module)  
+  .add('Doohicky', () => <Doohicky/>);
