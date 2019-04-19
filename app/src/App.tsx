9c9ad21d96a16eaf219dd1f4857b0687d74c418a
Andrew <andyerickson2014@gmail.com>
2019-04-18 22:35:08 -0700

    WIP: GH-76 add routing system for frontend

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,58 @@
 import * as React from 'react';
-import Doohicky from './components/Doohicky'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import HomePage from './components/Home/Home'
+// import Doohicky from './components/Doohicky'
 import './App.css';
 
-import logo from './logo.svg';
 
 class App extends React.Component {
+
+
   public render() {
+
+    let app = <Route render={ ({location}) => (
+      <>
+        <TransitionGroup className="smooth-container">
+          <CSSTransition
+            key={location.pathname}
+            timeout={{enter: 500, exit: 300}}
+            classNames="page"
+            appear
+            >
+            <Switch location={location}>
+              <Route
+                exact
+                path="/"
+                component={HomePage}/>
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      </>
+    )} />
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Doohicky />
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter> 
+          { app }
+      </BrowserRouter>
     );
+
   }
+  
+  // public render() {
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <h1 className="App-title">Welcome to React</h1>
+  //       </header>
+  //       <Doohicky />
+  //       <p className="App-intro">
+  //         To get started, edit <code>src/App.tsx</code> and save to reload.
+  //       </p>
+  //     </div>
+  //   );
+  // }
 }
 
 
---
+++ app/src/components/Home/Home.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+// import { connect } from 'react-redux'
+
+const HomePage = (props: any) => {
+
+  return (
+    <h1>Home</h1>
+  )
+}
+export default HomePage;
