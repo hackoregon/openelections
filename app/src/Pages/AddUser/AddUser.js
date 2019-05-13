6525e2c3d9f0fa4c021fb185ae35df09fc95632a
Andrew <andyerickson2014@gmail.com>
2019-05-13 16:48:29 -0700

    update: GH-88 add simple storybook ex

--- app/src/Pages/AddUser/AddUser.js
+++ app/src/Pages/AddUser/AddUser.js
@@ -28,24 +28,35 @@ const styles = theme => ({
 
 class AddUser extends React.Component {
   state = {
-    userRole: 'Staff',
-    firstName: '',
-    lastName: '',
-    email: ''
+    formValues: {
+      userRole: 'Staff',
+      firstName: '',
+      lastName: '',
+      email: ''
+    },
+    isSubmitted: false
   };
   handleStateChange(name, event) {
     console.log('change', name);
-    this.setState({ [name]: event.target.value });
+    this.setState({ formValues: {
+      ...this.state.formValues, 
+      [name]: event.target.value 
+    }});
   };
+  formIsSubmitted(bool) {
+    this.setState({isSubmitted: bool})
+  }
 
   clearState(e) {
     // e.preventDefault();
     console.log('clearing state')
     this.setState({
-      firstName: '',
-      lastName: '',
-      email: '',
-      userRole: 'Staff'
+      formValues: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        userRole: 'Staff'
+      }
     })
   }
 
@@ -60,25 +71,48 @@ class AddUser extends React.Component {
       <div className={classes.container}>
         <Paper elevation={1} className={classes.paper}>
         {console.log({state: this.state})}
+        <p style={{
+          fontSize: 35 + 'px',
+          letterSpacing: -2 + 'px',
+          margin: 10 + 'px ' + 0 + 'px' 
+          }}>Add a New User</p>
+        { !this.state.isSubmitted 
+          ? (
           <Formik
-            onSubmit={(values, actions) => {
-              console.log('Submitting: ', values, actions)
-            }}
-            onReset={ (values, bag) => {
-              console.log('on reset', {values}, {bag})
+          onSubmit={(values, actions) => {
+            console.log('Submitting: ', values, actions)
+            this.formIsSubmitted(true)
+          }}
+          onReset={ (values, bag) => {
+            console.log('on reset', {values}, {bag})
+            this.clearState()
+            bag.resetForm(this.state.formValues)
+          }}
+          render={props => (
+            <AddUserForm 
+              handleStateChange={this.handleStateChange.bind(this)} 
+              clearState={this.clearState.bind(this)} 
+              formValues={this.state.formValues}
+              {...{...props, userRoles }} 
+            />)}
+          initialValues={this.state.formValues}
+          validationSchema={validationSchema}
+        /> )
+        : (
+          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+            <svg width="188" height="188" viewBox="0 0 188 188" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M75.6095 123.65L50.0455 98.0856L41.3403 106.73L75.6095 140.999L149.175 67.4335L140.531 58.7896L75.6095 123.65Z" fill="#42B44A"/>
+              <circle cx="94" cy="94" r="91" stroke="#42B44A" strokeWidth="6"/>
+            </svg>
+            <p style={{textAlign: 'center'}}>An email was sent to {this.state.formValues.email}.</p>
+            <button onClick={() => {
+              this.formIsSubmitted(false)
               this.clearState()
-              bag.resetForm(this.state)
-            }}
-            render={props => (
-              <AddUserForm 
-                handleStateChange={this.handleStateChange.bind(this)} 
-                clearState={this.clearState.bind(this)} 
-                formValues={this.state}
-                {...{...props, userRoles }} 
-              />)}
-            initialValues={this.state}
-            validationSchema={validationSchema}
-          />
+            }}>close</button>
+          </div>
+          
+        )
+        }
         </Paper>
       </div>
     )
---
+++ app/stories/AddUser.story.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, object } from '@storybook/addon-knobs';
+import AddUser from '../src/Pages/AddUser/AddUser'
+
+export default () => storiesOf('AddUser', module)
+  .add('Add User Form', () => <AddUser />)
