c7b79b2217242b90aa5cff0e1327a0e099c1b8ea
Ian Harris <iankerriganharris@gmail.com>
2019-05-18 12:59:58 -0700

    Add government and permission seed fns

--- api/models/seeds/users.ts
+++ api/models/seeds/users.ts
@@ -1,19 +1,29 @@
-import {createUserAsync} from "../../services/userService";
+import { createUserAsync } from '../../services/userService';
+
+const seeds = [
+    {
+        email: 'dan@civicsoftwarefoundation.org',
+        password: 'password',
+        firstName: 'Dan',
+        lastName: 'Melton'
+    },
+    {
+        email: 'andrew.erickson@civicsoftwarefoundation.org',
+        password: 'password',
+        firstName: 'Andrew',
+        lastName: 'Erickson'
+    },
+    {
+        email: 'ian.harris@hackoregon.org',
+        password: 'password',
+        firstName: 'Ian',
+        lastName: 'Harris'
+    }
+];
 
 export default async () => {
     if (process.env.NODE_ENV !== 'development') {
         return console.log('Can only seed in development mode');
     }
-    await createUserAsync({
-        email: 'dan@civicsoftwarefoundation.org',
-        password: 'password',
-        firstName: 'Dan',
-        lastName: 'Melton'
-    });
-    await createUserAsync({
-        email: 'andrew.erickson@civicsoftwarefoundation.org',
-        password: 'password',
-        firstName: 'Andrew',
-        lastName: 'Erickson'
-    });
-}
+    return Promise.all(seeds.map(createUserAsync));
+};
