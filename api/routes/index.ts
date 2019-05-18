4a39dc4709bfe320708de114f0a3d821ecd351af
Ian Harris <iankerriganharris@gmail.com>
2019-05-18 11:55:59 -0700

    Add campaigns/new route

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,3 +1,7 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import { getCurrentUser, IRequest } from './helpers';
 import {
     login,
     invite,
@@ -5,19 +9,16 @@ import {
     getUsers,
     sendPasswordReset,
     resetPassword,
-    updatePassword, redeemInvite
+    updatePassword,
+    redeemInvite
 } from '../controller/users';
-import * as express from 'express';
-import { getCurrentUser, IRequest } from './helpers';
-import * as bodyParser from 'body-parser';
-import * as cookieParser from 'cookie-parser';
-import { Response } from 'express';
+import * as campaigns from '../controller/campaigns';
 
 export const AppRoutes = [
     {
         path: '/me',
         method: 'get',
-        action: async (request: IRequest, response: Response) => {
+        action: async (request: IRequest, response: express.Response) => {
             return response.status(200).json(request.currentUser);
         }
     },
@@ -59,7 +60,12 @@ export const AppRoutes = [
     {
         path: '/users/password',
         method: 'put',
-        action: updatePassword,
+        action: updatePassword
+    },
+    {
+        path: '/campaigns/new',
+        method: 'post',
+        action: campaigns.addCampaign
     }
 ];
 
@@ -69,7 +75,10 @@ export const setupRoutes = (app: express.Express) => {
     app.use(getCurrentUser);
     AppRoutes.forEach(route => {
         app[route.method](route.path, (request: IRequest, response: express.Response, next: Function) => {
-            route.action(request, response, next).then(() => next).catch(err => next(err));
+            route
+                .action(request, response, next)
+                .then(() => next)
+                .catch(err => next(err));
         });
     });
 };
