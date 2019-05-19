3927ed257dbfe3d5a21a78032b26aef67f6670aa
Andrew <andyerickson2014@gmail.com>
2019-05-19 12:54:28 -0700

    add submitted state ui

--- app/src/Pages/ForgotPassword/ForgotPassword.js
+++ app/src/Pages/ForgotPassword/ForgotPassword.js
@@ -64,26 +64,69 @@ class ForgotPassword extends Component {
         <div css={styles}>
           <Paper elevation={1} className="paper">
             <p className="form-title">Forgot Password</p>
-            <Formik
-              onSubmit={(values, actions) => {
-                console.log("Submitting: ", values, actions);
-                this.formIsSubmitted(true);
-              }}
-              onReset={(values, bag) => {
-                console.log("on reset", { values }, { bag });
-                this.clearState();
-                bag.resetForm(this.state.formValues);
-              }}
-              render={props => (
-                <ForgotPasswordForm
-                  handleStateChange={this.handleStateChange.bind(this)}
-                  clearState={this.clearState.bind(this)}
-                  {...props}
-                />
-              )}
-              initialValues={this.state.formValues}
-              validationSchema={validationSchema}
-            />
+            {!this.state.isSubmitted ? (
+              <Formik
+                onSubmit={(values, actions) => {
+                  console.log("Submitting: ", values, actions);
+                  this.formIsSubmitted(true);
+                }}
+                onReset={(values, bag) => {
+                  console.log("on reset", { values }, { bag });
+                  this.clearState();
+                  bag.resetForm(this.state.formValues);
+                }}
+                render={props => (
+                  <ForgotPasswordForm
+                    handleStateChange={this.handleStateChange.bind(this)}
+                    clearState={this.clearState.bind(this)}
+                    {...props}
+                  />
+                )}
+                initialValues={this.state.formValues}
+                validationSchema={validationSchema}
+              />
+            ) : (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center"
+                }}
+              >
+                <svg
+                  width="188"
+                  height="188"
+                  viewBox="0 0 188 188"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M75.6095 123.65L50.0455 98.0856L41.3403 106.73L75.6095 140.999L149.175 67.4335L140.531 58.7896L75.6095 123.65Z"
+                    fill="#42B44A"
+                  />
+                  <circle
+                    cx="94"
+                    cy="94"
+                    r="91"
+                    stroke="#42B44A"
+                    strokeWidth="6"
+                  />
+                </svg>
+                <p style={{ textAlign: "center" }}>
+                  An email was sent to {this.state.formValues.email} to reset
+                  your password.
+                </p>
+                <button
+                  onClick={() => {
+                    this.formIsSubmitted(false);
+                    this.clearState();
+                  }}
+                >
+                  close
+                </button>
+              </div>
+            )}
           </Paper>
         </div>
       </PageHoc>
