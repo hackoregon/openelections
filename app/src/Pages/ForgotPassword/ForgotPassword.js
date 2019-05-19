97a89faf9a35fa9630b5db9420103e2e0969ef56
Andrew <andyerickson2014@gmail.com>
2019-05-19 11:59:40 -0700

    GH-98: add forgot password page component

---
+++ app/src/Pages/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import * as Yup from "yup";
+import { Formik } from "formik";
+import Paper from "@material-ui/core/Paper";
+import PageHoc from "../../components/PageHoc/PageHoc";
+import { ForgotPasswordForm } from "../../components/Forms/ForgotPassword";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+const validationSchema = Yup.object({
+  email: Yup.string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required")
+});
+
+const styles = css`
+  max-width: 350px;
+  .paper {
+    margin-top: 30px,
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 40px 40px;
+  }
+  .form-title {
+    font-size: 35px;
+    letter-spacing: -2px;
+    margin: 10px 0px;
+  }
+`;
+
+class ForgotPassword extends Component {
+  state = {
+    formValues: {
+      email: ""
+    },
+    isSubmitted: false
+  };
+
+  handleStateChange(name, event) {
+    console.log("change", name);
+    this.setState({
+      formValues: {
+        ...this.state.formValues,
+        [name]: event.target.value
+      }
+    });
+  }
+  formIsSubmitted(bool) {
+    this.setState({ isSubmitted: bool });
+  }
+  clearState(e) {
+    // e.preventDefault();
+    console.log("clearing state");
+    this.setState({
+      formValues: {
+        email: ""
+      }
+    });
+  }
+  render() {
+    return (
+      <PageHoc>
+        <div css={styles}>
+          <Paper elevation={1} className="paper">
+            <p className="form-title">Forgot Password</p>
+            <Formik
+              onSubmit={(values, actions) => {
+                console.log("Submitting: ", values, actions);
+                this.formIsSubmitted(true);
+              }}
+              onReset={(values, bag) => {
+                console.log("on reset", { values }, { bag });
+                this.clearState();
+                bag.resetForm(this.state.formValues);
+              }}
+              render={props => (
+                <ForgotPasswordForm
+                  handleStateChange={this.handleStateChange.bind(this)}
+                  clearState={this.clearState.bind(this)}
+                  {...props}
+                />
+              )}
+              initialValues={this.state.formValues}
+              validationSchema={validationSchema}
+            />
+          </Paper>
+        </div>
+      </PageHoc>
+    );
+  }
+}
+export default ForgotPassword;
