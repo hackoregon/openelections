6253e50b72472f6ee477e2cd15da8ef09f2c4e7a
Ian Harris <iankerriganharris@gmail.com>
2019-05-24 23:06:56 -0700

    Add api methods and tests

--- app/src/api/api.js
+++ app/src/api/api.js
@@ -1,9 +1,9 @@
 import jwtDecode from "jwt-decode";
 
 export const UserRoleEnum = Object.freeze({
-  "GOVERNMENT_ADMIN": "government_admin",
-  "CAMPAIGN_ADMIN": "campaign_admin",
-  "CAMPAIGN_STAFF": "campaign_staff"
+  GOVERNMENT_ADMIN: "government_admin",
+  CAMPAIGN_ADMIN: "campaign_admin",
+  CAMPAIGN_STAFF: "campaign_staff"
 });
 
 export function baseUrl() {
@@ -28,12 +28,35 @@ export function me() {
   return get(`${baseUrl()}/me`).then(response => response.json());
 }
 
-export function inviteUsertoGovernment(email, firstName, lastName, governmentId) {
-  return post(`${baseUrl()}/users/invite`, { email, firstName, lastName, governmentId, role: UserRoleEnum.GOVERNMENT_ADMIN});
+export function inviteUsertoGovernment(
+  email,
+  firstName,
+  lastName,
+  governmentId
+) {
+  return post(`${baseUrl()}/users/invite`, {
+    email,
+    firstName,
+    lastName,
+    governmentId,
+    role: UserRoleEnum.GOVERNMENT_ADMIN
+  });
 }
 
-export function inviteUsertoCampaign(email, firstName, lastName, campaignId, role) {
-  return post(`${baseUrl()}/users/invite`, { email, firstName, lastName, campaignId, role});
+export function inviteUsertoCampaign(
+  email,
+  firstName,
+  lastName,
+  campaignId,
+  role
+) {
+  return post(`${baseUrl()}/users/invite`, {
+    email,
+    firstName,
+    lastName,
+    campaignId,
+    role
+  });
 }
 
 export function resendInvite(userId) {
@@ -52,15 +75,71 @@ export function redeemInvite(invitationCode, password, firstName, lastName) {
 }
 
 export function getCampaignUsers(campaignId) {
-  return post(`${baseUrl()}/users`, {campaignId}).then( response => response.json());
+  return post(`${baseUrl()}/users`, { campaignId }).then(response =>
+    response.json()
+  );
 }
 
 export function getGovernmentUsers(governmentId) {
-  return post(`${baseUrl()}/users`, {governmentId}).then( response => response.json());
+  return post(`${baseUrl()}/users`, { governmentId }).then(response =>
+    response.json()
+  );
+}
+
+//   path: '/users/send-password-reset-email',
+//   method: 'post',
+export function sendPasswordResetEmail(email) {
+  return post(`${baseUrl()}/users/send-password-reset-email`, { email });
+}
+
+//   path: '/users/reset-password',
+//   method: 'post',
+export function resetPassword(invitationCode, password) {
+  return post(`${baseUrl()}/users/reset-password`, {
+    invitationCode,
+    password
+  });
+}
+
+//   path: '/users/password',
+//   method: 'put',
+export function updatePassword(currentPassword, newPassword) {
+  return put(`${baseUrl()}/users/password`, { currentPassword, newPassword });
+}
+
+//   path: '/activities',
+//   method: 'post',
+export function getCampaignActivities(campaignId) {
+  return post(`${baseUrl()}/activities`, { campaignId }).then(response =>
+    response.json()
+  );
+}
+
+export function getGovernmentActivities(governmentId) {
+  return post(`${baseUrl()}/activities`, { governmentId }).then(response =>
+    response.json()
+  );
+}
+
+//   path: '/campaigns',
+//   method: 'post',
+export function getCampaignsForGovernment(governmentId) {
+  return post(`${baseUrl()}/campaigns`, { governmentId }).then(response =>
+    response.json()
+  );
+}
+
+//   path: '/campaigns/new',
+//   method: 'post',
+export function createCampaignForGovernment(governmentId, name) {
+  return post(`${baseUrl()}/campaigns/new`, { governmentId, name });
 }
 
 export function post(url, data) {
-  const headers = { "Content-Type": "application/json", Accept: "application/json" };
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  };
   if (process.env.NODE_ENV === "test" && !!process.env.TOKEN) {
     headers["Cookie"] = `token=${process.env.TOKEN}`;
   }
@@ -73,7 +152,10 @@ export function post(url, data) {
 }
 
 export function get(url) {
-  const headers = { "Content-Type": "application/json", Accept: "application/json" };
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  };
   if (process.env.NODE_ENV === "test" && !!process.env.TOKEN) {
     headers["Cookie"] = `token=${process.env.TOKEN}`;
   }
@@ -84,17 +166,22 @@ export function get(url) {
   });
 }
 
+export function put(url, data) {
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  };
+  if (process.env.NODE_ENV === "test" && !!process.env.TOKEN) {
+    headers["Cookie"] = `token=${process.env.TOKEN}`;
+  }
+  return fetch(url, {
+    method: "PUT",
+    headers,
+    body: JSON.stringify(data),
+    credentials: "include"
+  });
+}
+
 export function decodeToken(token) {
   return jwtDecode(token);
 }
-
-//   path: '/users',
-//   method: 'post',
-//   path: '/users/send-password-reset-email',
-//   method: 'post',
-//   path: '/users/reset-password',
-//   method: 'post',
-//   path: '/users/password',
-//   method: 'put',
-//   path: '/activities',
-//   method: 'post',
