057baa3e2a14f7dd6088112b77b55ab5a3962bf7
Andrew <andyerickson2014@gmail.com>
2019-06-24 20:34:46 -0700

    modify import to fix test

--- app/src/Pages/SignIn/SignIn.js
+++ app/src/Pages/SignIn/SignIn.js
@@ -1,45 +1,14 @@
 import React, { Component } from "react";
-import * as Yup from "yup";
-import { Formik } from "formik";
-import Paper from "@material-ui/core/Paper";
 import PageHoc from "../../components/PageHoc/PageHoc";
-import SignInForm from "../../components/Forms/SignIn";
+import SignInForm from "../../components/Forms/SignIn/index";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
-const validationSchema = Yup.object({
-  email: Yup.string("Enter your email")
-    .email("Enter a valid email")
-    .required("Email is required"),
-  password: Yup.string("What was you password").required(
-    "What was your password"
-  )
-});
-
-const styles = css`
-  max-width: 350px;
-  .paper {
-    margin-top: 30px,
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 40px 40px;
-  }
-  .form-title {
-    font-size: 35px;
-    letter-spacing: -2px;
-    margin: 10px 0px;
-  }
-`;
-
 class SignIn extends Component {
-
   render() {
     return (
       <PageHoc>
-        <div css={styles}>
-          <SignInForm />
-        </div>
+        <SignInForm />
       </PageHoc>
     );
   }
