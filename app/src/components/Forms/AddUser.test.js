02edbfdda499ae9d6e639c0d3fc966a8bf70bf53
Belal Sejouk <billoosijok@gmail.com>
2019-06-24 20:03:05 -0700

    Git remove form files

--- app/src/components/Forms/AddUser.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { AddUserForm } from "./AddUser";
-
-describe("<AddUserForm/>", () => {
-  it("should be defined", () => {
-    expect(AddUserForm).toMatchSnapshot();
-  });
-});
