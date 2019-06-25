de2594f558ac4514160066606a8d313ef1d38a71
Dan Melton <melton.dan@gmail.com>
2019-06-25 08:58:35 -0700

    adds all production docker stuffz

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,28 +1,8 @@
 #! /bin/bash
-usage() { echo "Usage: $0 [-d] for a development build, [-p] for a production build" 1>&2; exit 1; }
 
-if [ $# == 0 ]; then usage; fi
-
-while getopts ":dp" opt; do
-    case "$opt" in
-        d)
-          if [ `uname -s` = "Linux" ]
-          then
-            sudo docker-compose build --build-arg DEBUG=true api app
-          else
-            docker-compose build --build-arg DEBUG=true api app
-          fi
-          ;;
-        p)
-          if [ `uname -s` = "Linux" ]
-          then
-            sudo docker-compose build api app
-          else
-            docker-compose build api app
-          fi
-          ;;
-        *)x
-          usage
-          ;;
-    esac
-done
+if [ `uname -s` = "Linux" ]
+then
+sudo docker-compose  -f docker-compose-production.yml build api app
+else
+docker-compose -f docker-compose-production.yml build api app
+fi
