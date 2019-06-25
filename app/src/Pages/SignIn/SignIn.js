a71adc8fd99b9af832b89790fcf4c7d6f5c49b2c
Belal Sejouk <billoosijok@gmail.com>
2019-06-24 20:11:54 -0700

    Forgot to stage one file

--- app/src/Pages/SignIn/SignIn.js
+++ app/src/Pages/SignIn/SignIn.js
@@ -3,7 +3,7 @@ import * as Yup from "yup";
 import { Formik } from "formik";
 import Paper from "@material-ui/core/Paper";
 import PageHoc from "../../components/PageHoc/PageHoc";
-import { SignInForm } from "../../components/Forms/SignIn";
+import SignInForm from "../../components/Forms/SignIn";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
@@ -33,63 +33,12 @@ const styles = css`
 `;
 
 class SignIn extends Component {
-  state = {
-    formValues: {
-      email: "",
-      password: ""
-    },
-    isSubmitted: false
-  };
 
-  handleStateChange(name, event) {
-    console.log("change", name);
-    this.setState({
-      formValues: {
-        ...this.state.formValues,
-        [name]: event.target.value
-      }
-    });
-  }
-  formIsSubmitted(bool) {
-    this.setState({ isSubmitted: bool });
-  }
-  clearState(e) {
-    // e.preventDefault();
-    console.log("clearing state");
-    this.setState({
-      formValues: {
-        email: "",
-        password: ""
-      }
-    });
-  }
   render() {
     return (
       <PageHoc>
         <div css={styles}>
-          <Paper elevation={1} className="paper">
-            <p className="form-title">Sign In</p>
-            <Formik
-              onSubmit={(values, actions) => {
-                console.log("Submitting: ", values, actions);
-                this.formIsSubmitted(true);
-              }}
-              onReset={(values, bag) => {
-                console.log("on reset", { values }, { bag });
-                this.clearState();
-                bag.resetForm(this.state.formValues);
-              }}
-              render={props => (
-                <SignInForm
-                  handleStateChange={this.handleStateChange.bind(this)}
-                  clearState={this.clearState.bind(this)}
-                  {...props}
-                />
-              )}
-              initialValues={this.state.formValues}
-              validationSchema={validationSchema}
-            />
-          </Paper>
+          <SignInForm />
         </div>
       </PageHoc>
     );
