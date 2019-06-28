ba3d22cdd4e3ff97ccac78b399f523d7ba4c9f42
Garret James <git@garretjames.com>
2019-06-27 22:33:14 -0700

    Patch to #345

--- app/src/components/Forms/Signup.js
+++ app/src/components/Forms/Signup.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as Yup from "yup";
-import Form from "./Form";
+import Form from "../Form/Form";
 import PasswordField from "../Fields/PasswordField";
 
 	const fields = {
