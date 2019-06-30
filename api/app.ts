a8ae1926dc09a5378c08263d24b3aa441163bb3f
Michael Scotto di Carlo <mscotto@supportland.com>
2019-06-30 12:28:49 -0700

    Adds cors response from api to specify requesting domain as allowed domain

--- api/app.ts
+++ api/app.ts
@@ -4,12 +4,24 @@ import * as cors from 'cors';
 import { setupRoutes } from './routes';
 import db from './models/db';
 
+/*** 
+    corsOptions set to return all Access-Control-Allow-Origin headers with requesting domain
+    Solves for credentialed cross domain request that will not accept * wildcard 
+    See link to create a white list:
+        https://expressjs.com/en/resources/middleware/cors.html#configuring-cors-w-dynamic-origin
+***/
+let corsOptions = {
+  origin: function (origin, callback) {
+      callback(null, true);
+  },
+  credentials: true
+};
 
 (async () => {
     await db();
     const app: express.Express = express();
     app.use(logger('dev'));
-    app.use(cors());
+    app.use(cors(corsOptions));
     setupRoutes(app);
     return app.listen(3000);
 })();
