2cedb39d10aed9b5e584efe01026de278546e337
Kelly de Vries <kdv@me.com>
2019-07-05 22:17:11 -0700

    add initial formSections

--- app/src/components/Forms/AddContribution/AddContributionForm.js
+++ app/src/components/Forms/AddContribution/AddContributionForm.js
@@ -6,39 +6,80 @@ import TextField from "../../Fields/TextField";
 import SelectField from "../../Fields/SelectField";
 
 const fields = {
-  userRole: {
-    label: "Role",
-    section: "addUserRole",
+  dateOfContribution: {
+    label: "Date of Contribution",
+    section: "dateOfContribution",
+    component: TextField,
+    validation: Yup.date("Enter date of contribution").required(
+      "A contribution date is required"
+    )
+  },
+  typeOfContribution: {
+    label: "Type of Contribution",
+    section: "typeOfContribution",
+    component: SelectField,
     options: {
       values: ["Admin", "Staff"] // get from Redux state eventually
     },
+    validation: Yup.string("Choose the type of contribution")
+      .required("A contribution type is required")
+  },
+  subTypeOfContribution: {
+    label: "Subtype of Contribution",
+    section: "subTypeOfContribution",
     component: SelectField,
-    validation: Yup.string("Choose a user role").required(
-      "A user role is required"
-    )
+    options: {
+      values: ["Admin", "Staff"] // get from Redux state eventually
+    },
+    validation: Yup.string("Choose the subtype of contribution")
+      .required("The contribution subtype is required") // KELLY - is the subtype required?
   },
-  email: {
-    label: "Email",
-    section: "addUser",
-    component: TextField,
-    validation: Yup.string("Enter your email")
-      .email("Enter a valid email")
-      .required("Email is required")
+  typeOfContributor: {
+    label: "Type of Contributor",
+    section: "typeOfContributor",
+    component: SelectField,
+    options: {
+      values: ["Admin", "Staff"] // get from Redux state eventually
+    },
+    validation: Yup.string("Choose the type of contributor")
+      .required("A contributor type is required")
   },
-  firstName: {
-    label: "First Name",
-    section: "addUser",
-    component: TextField,
-    validation: Yup.string("Enter your first name").required(
-      "First Name is required"
-    )
+  amountOfContribution: {
+    label: "Amount of Contribution",
+    section: "amountOfContribution",
+    component: SelectField,
+    options: {
+      values: ["Admin", "Staff"] // get from Redux state eventually
+    },
+    validation: Yup.string("Choose the amount of contribution")
+      .required("The contribution amount is required") 
   },
-  lastName: {
-    label: "Last Name",
-    section: "addUser",
+  oaeContributionType: {
+    label: "OAE Contribution Type",
+    section: "oaeContributionType",
+    component: SelectField,
+    options: {
+      values: ["Admin", "Staff"] // get from Redux state eventually
+    },
+    validation: Yup.string("Choose the OAE contribution type")
+      .required("The OAE contribution type is required") 
+  },
+  paymentMethod: {
+    label: "Payment Method",
+    section: "paymentMethod",
+    component: SelectField,
+    options: {
+      values: ["Admin", "Staff"] // get from Redux state eventually
+    },
+    validation: Yup.string("Choose the payment method")
+      .required("The payment method is required") 
+  },
+  checkNumber: {
+    label: "Check Number",
+    section: "checkNumber",
     component: TextField,
-    validation: Yup.string("Enter your last name").required(
-      "Last Name is required"
+    validation: Yup.string("Enter your check number").required(
+      "Check number is required"
     )
   }
 };
@@ -46,7 +87,7 @@ const fields = {
 const AddContributionForm = ({ initialValues, onSubmit, children }) => (
   <Form
     fields={fields}
-    sections={["addUserRole", "addUser"]}
+    sections={["dateOfContribution", "typeOfContribution", "subTypeOfContribution", "typeOfContributor", "amountOfContribution", "oaeContributionType", "paymentMethod", "checkNumber"]}
     initialValues={initialValues}
     onSubmit={onSubmit}
   >
