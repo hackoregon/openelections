1fd007ac249dd8f2789126a9161cfab498c141f5
Kelly de Vries <kdv@me.com>
2019-07-07 11:23:30 -0700

    add test for AddContribution page

---
+++ app/src/Pages/AddContribution/AddContribution.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+import AddContribution from "./AddContribution";
+
+describe("<AddContribution/>", () => {
+  it("should be defined", () => {
+    const wrapper = shallow(<AddContribution />);
+    expect(wrapper).toBeDefined();
+    expect(wrapper).toMatchSnapshot();
+  });
+});
