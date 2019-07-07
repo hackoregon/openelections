cc6d028474b671216155b3101f4f5ccdbd30140d
Dan Melton <melton.dan@gmail.com>
2019-07-07 14:08:50 -0700

    modifying image names for production deploys

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-if [ `uname -s` = "Linux" ]
-then
-sudo docker-compose  -f docker-compose-production.yml build api app
-else
-docker-compose -f docker-compose-production.yml build api app
-fi
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,22 +4,24 @@ set -e
 
 export PATH=$PATH:$HOME/.local/bin
 
+docker-compose  -f docker-compose-production.yml build api app
+
 echo Getting the ECR login...
 eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
 
 # tag with branch and travis build number then push
 GITTAG=$(git rev-parse --short HEAD)
 echo Tagging with "GITTAG"
-docker tag openelections_api:latest 845828040396.dkr.ecr.us-west-2.amazonaws.com/openelections-api:"$GITTAG"
-docker tag  openelections_app:latest 845828040396.dkr.ecr.us-west-2.amazonaws.com/openelections-app:"$GITTAG"
+docker tag openelections-api-production:latest 845828040396.dkr.ecr.us-west-2.amazonaws.com/openelections-api:"$GITTAG"
+docker tag  openelections-app-production:latest 845828040396.dkr.ecr.us-west-2.amazonaws.com/openelections-app:"$GITTAG"
 docker push 845828040396.dkr.ecr.us-west-2.amazonaws.com/openelections-api:"$GITTAG"
 docker push 845828040396.dkr.ecr.us-west-2.amazonaws.com/openelections-app:"$GITTAG"
 
 # tag with "latest" then push
 TAG="latest"
 echo Tagging with "$TAG"
-docker tag openelections_api:latest 845828040396.dkr.ecr.us-west-2.amazonaws.com/openelections-api:"$TAG"
-docker tag  openelections_app:latest 845828040396.dkr.ecr.us-west-2.amazonaws.com/openelections-app:"$TAG"
+docker tag openelections-api-production:latest 845828040396.dkr.ecr.us-west-2.amazonaws.com/openelections-api:"$TAG"
+docker tag  openelections-app-production:latest 845828040396.dkr.ecr.us-west-2.amazonaws.com/openelections-app:"$TAG"
 docker push 845828040396.dkr.ecr.us-west-2.amazonaws.com/openelections-api:"$TAG"
 docker push 845828040396.dkr.ecr.us-west-2.amazonaws.com/openelections-app:"$TAG"
 
