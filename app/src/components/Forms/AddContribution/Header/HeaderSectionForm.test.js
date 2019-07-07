cf6365764c859084b44189bbade4b3088419c476
Kelly de Vries <kdv@me.com>
2019-07-07 16:54:04 -0700

    rename header section & add test

---
+++ app/src/components/Forms/AddContribution/Header/HeaderSectionForm.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import HeaderSectionForm from "./HeaderSectionForm";
+
+describe("<HeaderSectionForm/>", () => {
+  it("should be defined", () => {
+    expect(HeaderSectionForm).toMatchSnapshot();
+  });
+});
