cae833849d6d31e7c73002b5cdf39e82f22e6acb
Kelly de Vries <kdv@me.com>
2019-07-07 15:55:57 -0700

    remove initial AddContributionForm

--- app/src/components/Forms/AddContribution/AddContributionForm.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AddContributionForm from "./AddContributionForm";
-
-describe("<AddContributionForm/>", () => {
-  it("should be defined", () => {
-    expect(AddContributionForm).toMatchSnapshot();
-  });
-});
