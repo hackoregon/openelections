4d517750eb6baf129e92e9eaf8d333d9b7e6358c
Kelly de Vries <kdv@me.com>
2019-07-08 16:01:08 -0700

    move all AddContribution components into one with single index & delete old components

--- app/src/components/Forms/AddContribution/OtherDetailsSection/OtherDetailsSectionForm.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import OtherDetailsSectionForm from "./OtherDetailsSectionForm";
-
-describe("<OtherDetailsSectionForm/>", () => {
-  it("should be defined", () => {
-    expect(OtherDetailsSectionForm).toMatchSnapshot();
-  });
-});
