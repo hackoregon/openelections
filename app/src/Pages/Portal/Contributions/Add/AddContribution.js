eeeefd5a902aca3482ea4fdf85ac09bb817101e2
Kelly de Vries <kdv@me.com>
2019-07-07 17:26:14 -0700

    removed unused code & change initial values to "" for now

--- app/src/Pages/Portal/Contributions/Add/AddContribution.js
+++ app/src/Pages/Portal/Contributions/Add/AddContribution.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 import PageHoc from "../../../../components/PageHoc/PageHoc";
 import AddHeaderSectionForm from "../../../../components/Forms/AddContribution/Header/index";
 import AddBasicsSectionForm from "../../../../components/Forms/AddContribution/BasicsSection/index";
-import AddContributorSectionForm from "../../../../components/Forms/AddContribution/ContributorSection/index"
-import OtherDetailsSectionForm from "../../../../components/Forms/AddContribution/OtherDetailsSection/index";
+import AddContributorSectionForm from "../../../../components/Forms/AddContribution/ContributorSection/index";
+import AddOtherDetailsSectionForm from "../../../../components/Forms/AddContribution/OtherDetailsSection/index";
 import { connect } from "react-redux";
 import { login } from "../../../../state/ducks/auth";
 
 class AddContribution extends Component {
-  componentWillUpdate(newprops){
-    if (!(typeof newprops.state.me == 'undefined' || !newprops.state.me)) {
-      this.props.history.push('/dashboard')
+  componentWillUpdate(newprops) {
+    if (!(typeof newprops.state.me == "undefined" || !newprops.state.me)) {
+      this.props.history.push("/dashboard");
     }
   }
   render() {
@@ -19,20 +19,20 @@ class AddContribution extends Component {
         <AddHeaderSectionForm {...this.props} />
         <AddBasicsSectionForm {...this.props} />
         <AddContributorSectionForm {...this.props} />
-        <OtherDetailsSectionForm {...this.props} />
+        <AddOtherDetailsSectionForm {...this.props} />
       </PageHoc>
     );
   }
 }
 
 export default connect(
-  state => { 
-    return {state: state.auth}
-  }, 
+  state => {
+    return { state: state.auth };
+  },
   dispatch => {
     return {
-      login: (email,password) => dispatch(login(email,password)),
+      login: (email, password) => dispatch(login(email, password)),
       dispatch
-      }
-    }
-  )(AddContribution);
+    };
+  }
+)(AddContribution);
