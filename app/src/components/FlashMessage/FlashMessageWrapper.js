ef2dd33e04eb209c74231682cd42ed8254cf2b7c
Michael Scotto di Carlo <mscotto@supportland.com>
2019-07-08 16:43:01 -0700

    Clean up FlashMessageWrapper.js

--- app/src/components/FlashMessage/FlashMessageWrapper.js
+++ app/src/components/FlashMessage/FlashMessageWrapper.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import Button from '@material-ui/core/Button';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import ErrorIcon from '@material-ui/icons/Error';
 import InfoIcon from '@material-ui/icons/Info';
 import CloseIcon from '@material-ui/icons/Close';
 import { amber, green, yellow } from '@material-ui/core/colors';
 import IconButton from '@material-ui/core/IconButton';
-import Snackbar from '@material-ui/core/Snackbar';
 import SnackbarContent from '@material-ui/core/SnackbarContent';
 import WarningIcon from '@material-ui/icons/Warning';
 import { makeStyles } from '@material-ui/styles';
-// import { ThemeProvider } from '@material-ui/core/styles';
-// <ThemeProvider theme={theme} >
-// </ThemeProvider>
-
- const theme = {};
-
 
 const variantIcon = {
   success: CheckCircleIcon,
