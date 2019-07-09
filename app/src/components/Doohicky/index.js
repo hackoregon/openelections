dad9999009e00f96a820ee7a388391b60cd5363d
Andrew <andyerickson2014@gmail.com>
2019-07-08 20:59:00 -0700

    address failed test

--- app/src/components/Doohicky/index.js
+++ app/src/components/Doohicky/index.js
@@ -1,25 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
-import { getCurrentState } from "../../state/test/selectors";
-import { testEmitter } from "../../state/test/actions";
 const doohicky = props => {
   console.log(props.data);
   return (
     <div>
-      <h1>Testing: {props.data.text}</h1>
-      <button onClick={() => props.testSend("what?")}>Click</button>
+      <h1>Testing: Dohicky</h1>
+      <button onClick={() => console.log("what?")}>Click</button>
     </div>
   );
 };
 
-export default connect(
-  state => ({
-    data: getCurrentState(state)
-  }),
-  dispatch => ({
-    testSend(message) {
-      console.log(message);
-      dispatch(testEmitter(message));
-    }
-  })
-)(doohicky);
+export default connect()(doohicky);
