c232fdc693c65a4d1e2f9df771f255d53956decf
Michael Scotto di Carlo <mscotto@supportland.com>
2019-07-09 16:32:51 -0700

    Added variant to change status type

--- app/src/components/FlashMessage/FlashMessage.js
+++ app/src/components/FlashMessage/FlashMessage.js
@@ -5,41 +5,50 @@ import { getLatestMessage } from 'redux-flash';
 import Snackbar from '@material-ui/core/Snackbar';
 import SnackbarContentWrapper from "./FlashMessageWrapper";
 
-/*
-To use on a connected component
-import { flashMessage } from 'redux-flash';
+/***
+ 
+  Use on a connected component like so:
+  import { flashMessage } from 'redux-flash';
 
-const options = {
-  props: {
-    variant: 'success',         //one of: success, warning, error, info
-    someOtherProp: 'someValue'  //Pipe in other MUI snackbar props. See: https://material-ui.com/api/snackbar/
-  }
-
-}
-
-props.flashMessage('Signin Success', options);
-
- ...
-// mapDispatchToProps: 
-dispatch => {
-    return {
-      flashMessage: (message, options) => dispatch(flashMessage(message, options))
+  # Set variant, it takes: success, warning, error, or info. Defaults to success.
+   const options = {
+    props: {
+      variant: 'success',         
     }
   }
-*/
 
+  # Dispatch flashMessage(message, [options])
+  flashMessage('Wow so successful', options);
 
+    - variant is only the option currently being passed
+    - Others available, see https://material-ui.com/api/snackbar/
+   const options = {
+    props: {
+      variant: 'success',         
+    }
+  }
+
+  // mapDispatchToProps 
+  dispatch => {
+      return {
+        flashMessage: (message, options) => dispatch(flashMessage(message, options))
+      }
+    }
+
+***/
 
 function FlashMessage ({ flash }) {
   return (
     <div  >
     {
       flash && 
-      <div>
+      <div x={console.log(flash)}>
       <Snackbar open={true}  >  
         <SnackbarContentWrapper 
            variant = {flash.props.variant ? flash.props.variant : "success"}  
+           anchororigin = {flash.props.anchorOrigin}
            message = {flash.message}
+           className = 'anchorOriginTopRight'
         />
         </Snackbar> 
      </div>
