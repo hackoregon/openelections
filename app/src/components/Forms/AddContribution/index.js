9e9c76574a69cf4de9454c97e739672ed4c3c879
Kelly de Vries <kdv@me.com>
2019-07-09 08:34:54 -0700

    WIP- add initial flexbox styling

--- app/src/components/Forms/AddContribution/index.js
+++ app/src/components/Forms/AddContribution/index.js
@@ -6,50 +6,48 @@ import { css, jsx } from "@emotion/core";
 import { isLoggedIn } from "../../../state/ducks/auth";
 import { connect } from "react-redux";
 
-const invoice = css`
-  font-family: SofiaProRegular;
-  font-size: 32px;
-  line-height: 37px;
-  color: #333333;
-`;
+const headerStyles = {
+  invoice: css`
+    font-family: SofiaProRegular;
+    font-size: 32px;
+    line-height: 37px;
+    color: #333333;
+  `,
+  rightColumn: css`
+    display: flex;
+    justifycontent: right;
+  `,
 
-const rightColumn = css`
-  display: flex;
-  justifycontent: right;
-`;
+  status: css`
+    font-family: SofiaProRegular;
+    font-size: 13px;
+    line-height: 15px;
+    color: #979797;
+  `,
 
-const status = css`
-  font-family: SofiaProRegular;
-  font-size: 13px;
-  line-height: 15px;
-  color: #979797;
-`;
-
-const actualStatus = css`
-  font-family: SofiaProRegular;
-  font-size: 21px;
-  line-height: 25px;
-  color: #000000;
-`;
-
-const submitButton = css`
-  background: #d8d8d8;
-  border-radius: 5px;
-  color: white;
-`;
-
-const statusBlock = css`
-  display: flex;
-  flexdirection: column;
-  alignitems: left;
-`;
-
-const subheading = css`
-  font-family: SofiaProRegular;
-  font-size: 16px;
-  line-height: 19px;
-  color: #333333;
-`;
+  actualStatus: css`
+    font-family: SofiaProRegular;
+    font-size: 21px;
+    line-height: 25px;
+    color: #000000;
+  `,
+  submitButton: css`
+    background: #d8d8d8;
+    border-radius: 5px;
+    color: white;
+  `,
+  statusBlock: css`
+    display: flex;
+    flexdirection: column;
+    alignitems: left;
+  `,
+  subheading: css`
+    font-family: SofiaProRegular;
+    font-size: 16px;
+    line-height: 19px;
+    color: #333333;
+  `
+};
 
 const sectionTitle = css`
   font-family: Rubik;
@@ -57,10 +55,11 @@ const sectionTitle = css`
   font-weight: normal;
   font-size: 24px;
   line-height: 28px;
+  width: 100%;
   color: #000000;
 `;
 
-const sections = css`
+const sectionFields = css`
   font-family: Rubik;
   font-style: normal;
   font-weight: normal;
@@ -71,8 +70,22 @@ const sections = css`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content: space-around;
   background-color: green;
+`;
+
+const fieldStyle = css`
+  background-color: red;
+  width: 40%;
+  color: orange;
+  margin: 2px;
+`;
+
+const basicsStyles = css`
+  background-color: hotpink;
+  margin: 2%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
 `;
 
 // HEADER PIECES
@@ -121,21 +134,27 @@ const AddContribution = () => (
       notes: ""
     }}
   >
-    {({ formSections, isValid, handleSubmit /* isDirty, isSubmitting */ }) => (
+    {({
+      formSections,
+      formFields,
+      isValid,
+      handleSubmit /* isDirty, isSubmitting */
+    }) => (
       <React.Fragment>
-        <p css={invoice}>{invoiceNumber}</p>
-        <div css={subheading}>
+        {/* HEADER SECTION */}
+        <p css={headerStyles.invoice}>{invoiceNumber}</p>
+        <div css={headerStyles.subheading}>
           <p>
             {`${campaignName} Campaign`} | {`Last Edited ${lastEdited}`}
           </p>
         </div>
-        <div css={rightColumn}>
-          <div css={statusBlock}>
-            <p css={status}>Current Status</p>
-            <p css={actualStatus}>{currentStatus}</p>
+        <div css={headerStyles.rightColumn}>
+          <div css={headerStyles.statusBlock}>
+            <p css={headerStyles.status}>Current Status</p>
+            <p css={headerStyles.actualStatus}>{currentStatus}</p>
           </div>
           <Button
-            css={submitButton}
+            css={headerStyles.submitButton}
             buttonType="submit"
             disabled={!isValid}
             onClick={handleSubmit}
@@ -147,8 +166,19 @@ const AddContribution = () => (
         <p>{`+ Add Labels ${addALabel}`}</p>
 
         {/* BASICS SECTION */}
-        <h3 css={sectionTitle}>Basics</h3>
-        <div css={sections}>{formSections.basicsSection}</div>
+        <div css={basicsStyles}>
+          <h3 css={sectionTitle}>Basics</h3>
+          <div css={sectionFields}>
+            <h2 css={fieldStyle}>{formFields.dateOfContribution}</h2>
+            <h2 css={fieldStyle}>{formFields.typeOfContribution}</h2>
+            <h2 css={fieldStyle}>{formFields.subTypeOfContribution}</h2>
+            <h2 css={fieldStyle}>{formFields.amountOfContribution}</h2>
+            <h2 css={fieldStyle}>{formFields.oaeContributionType}</h2>
+            <h2 css={fieldStyle}>{formFields.paymentMethod}</h2>
+            <h2 css={fieldStyle}>{formFields.checkNumber}</h2>
+          </div>
+        </div>
+
         <p css={sectionTitle}>Contributor</p>
         <div>{formSections.contributorSection}</div>
         <p css={sectionTitle}>Other Details</p>
