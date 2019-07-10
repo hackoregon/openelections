75254131be2836793f243ade6cbe3d3ba19ba846
Kelly de Vries <kdv@me.com>
2019-07-10 14:10:49 -0700

    create new field without underline for displaying values

---
+++ app/src/components/Fields/FieldValue.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import FieldValue from "./FieldValue";
+
+describe("<FieldValue/>", () => {
+  it("should be defined", () => {
+    expect(FieldValue).toMatchSnapshot();
+  });
+});
