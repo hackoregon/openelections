bcc6db960d183c3c1ee48d953790c0a64d42df94
Punkael <punkael@pobox.com>
2019-07-10 12:30:21 -0700

    add DateField component (#392)

---
+++ app/src/components/Fields/DateField.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TextFieldMaterial from "@material-ui/core/TextField";
+
+const DateField = ({ id, label, formik }) => (
+  <TextFieldMaterial
+    id={id}
+    name={id}
+    label={label}
+    type="date"
+    helperText={formik.touched[id] ? formik.errors[id] : ""}
+    error={formik.touched[id] && Boolean(formik.errors[id])}
+    value={formik.values[id]}
+    onChange={formik.handleChange}
+    onBlur={formik.handleBlur}
+    autoComplete="on"
+    fullWidth
+  />
+);
+
+DateField.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string,
+  formik: PropTypes.shape({})
+};
+
+export default DateField;
