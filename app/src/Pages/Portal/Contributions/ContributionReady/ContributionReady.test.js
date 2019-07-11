394adb2c354b3da410d2f2dc5dc5920329d58830
Kelly de Vries <kdv@me.com>
2019-07-11 14:59:51 -0700

    add ContributionReady form, index, page, test, routes

---
+++ app/src/Pages/Portal/Contributions/ContributionReady/ContributionReady.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+import ContributionReady from "./ContributionReady";
+
+describe("<ContributionReady/>", () => {
+  it("should be defined", () => {
+    const wrapper = shallow(<ContributionReady />);
+    expect(wrapper).toBeDefined();
+    expect(wrapper).toMatchSnapshot();
+  });
+});
