640483275901ed1904a4d924a759f8e0803e07f2
Kelly de Vries <kdv@me.com>
2019-07-11 10:29:34 -0700

    clearn up with refactored format from AddExpense

--- app/src/components/Forms/AddContribution/index.js
+++ app/src/components/Forms/AddContribution/index.js
@@ -6,136 +6,190 @@ import { css, jsx } from "@emotion/core";
 import { isLoggedIn } from "../../../state/ducks/auth";
 import { connect } from "react-redux";
 
-const container = css`
-  width: 96%%;
-  min-height: 100%;
-  display: grid;
-  grid-template-rows: repeat(auto-fit, minmax(15px, 1fr));
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  grid-gap: 20px;
-`;
-
-const contributorContainer = css`
-  width: 96%%;
-  min-height: 25px;
-  display: grid;
-  grid-template-rows: repeat(auto-fit, minmax(15px, 1fr));
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  grid-gap: 20px;
-`;
-
-const addressContainer = css`
-  width: 96%%;
-  min-height: 25px;
-  display: grid;
-  grid-template-rows: repeat(auto-fit, minmax(15px, 1fr));
-  grid-template-columns: 1fr;
-  grid-gap: 20px;
-`;
-
-const cityStateZip = css`
-  width: 96%%;
-  min-height: 25px;
-  display: grid;
-  grid-template-rows: repeat(auto-fit, minmax(15px, 1fr));
-  grid-template-columns: 2fr 22% 24%;
-  grid-gap: 20px;
-`;
+const containers = {
+  header: css`
+    width: 96%%;
+    min-height: 100%;
+    display: grid;
+    grid-template-rows: repeat(auto-fit(15px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+    grid-gap: 20px;
+    margin-right: 38px;
+  `,
+  main: css`
+    display: grid;
+    grid-template-rows: repeat(auto-fit(50px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 20px;
+    margin-bottom: 20px;
+  `,
+  sectionTwo: css`
+    display: grid;
+    grid-template-rows: repeat(auto-fit(50px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 20px;
+    margin-bottom: 20px;
+  `,
+  fullWidth: css`
+    display: grid;
+    grid-template-rows: repeat(auto-fit(50px, 1fr));
+    grid-template-columns: 1fr;
+    grid-gap: 20px;
+    margin-bottom: 20px;
+  `,
+  cityStateZip: css`
+    width: 96%%;
+    min-height: 25px;
+    display: grid;
+    grid-template-rows: repeat(auto-fit, minmax(15px, 1fr));
+    grid-template-columns: 2fr 22% 24%;
+    grid-gap: 20px;
+    margin-bottom: 20px;
+  `
+};
 
 const headerStyles = {
   header: css`
     display: flex;
     justify-content: space-between;
+    margin-right: 38px;
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: normal;
   `,
   leftColumn: css`
-    color: red;
+    margin-right: 70px;
+  `,
+  rightColumn: css`
+    display: flex;
+    flex-direction: row-reverse;
   `,
   invoice: css`
-    font-family: SofiaProRegular;
-    font-size: 32px;
-    line-height: 37px;
+    font-size: 48px;
+    line-height: 57px;
+    margin: 0px;
+    /* identical to box height */
     color: #333333;
-    margin-bottom: 0px;
   `,
   subheading: css`
-    font-family: SofiaProRegular;
     font-size: 16px;
     line-height: 19px;
-    color: #333333;
+    margin-top: 0px;
+  `,
+  labelBlock: css`
+    margin-right: 40px;
   `,
   labels: css`
-    font-family: SofiaProRegular;
     font-size: 13px;
     line-height: 15px;
     color: #979797;
     margin-bottom: 4px;
   `,
-  addLabels: css`
-    font-family: SofiaProRegular;
+  smallBlueText: css`
     font-size: 13px;
     line-height: 15px;
-    margin-top: 0px;
+    margin: 0px;
     /* Link */
     color: #5f5fff;
   `,
-  rightColumn: css`
-    display: flex;
-    justify-content: right;
-    flex-direction: column;
-    margin-right: 38px;
+  largerBlueText: css`
+    font-size: 16px;
+    line-height: 19px;
+    margin: 0px;
+    /* Link */
+    color: #5f5fff;
   `,
-
   status: css`
-    font-family: SofiaProRegular;
     font-size: 13px;
     line-height: 15px;
     color: #979797;
     margin-bottom: 4px;
   `,
   actualStatus: css`
-    font-family: SofiaProRegular;
     font-size: 21px;
     line-height: 25px;
     color: #000000;
     margin-top: 0px;
-  `,
-  submitButton: css`
-    background: #d8d8d8;
-    border-radius: 5px;
-    color: white;
-    width: 243px;
+    margin-bottom: 20px;
   `,
   statusBlock: css`
-    display: flex;
     flex-direction: column;
     align-items: left;
+  `,
+  buttonDiv: css`
+    display: flex;
+    justify-content: space-between;
+    align-self: flex-end;
+  `,
+  submitButton: css`
+    background-color: #d8d8d8;
+    border-radius: 5px;
+    color: white;
+    width: 165px;
+    height: 50px;
+  `,
+  draftButton: css`
+    // HAVING ZERO IMPACT
+    background-color: #5f5fff;
+    border-radius: 5px;
+    width: 165px;
+    height: 50px;
   `
 };
 
-const sectionTitle = css`
-  font-family: Rubik;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 24px;
-  line-height: 28px;
-  width: 100%;
-  color: #000000;
-  margin-top: 55px;
-`;
+const sectionStyles = {
+  main: css`
+    margin-right: 34px;
+    margin-bottom: 34px;
+    margin-top: 34px;
+  `,
+  dividerLine: css`
+    margin-left: -20px;
+  `,
+  title: css`
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 28px;
+    width: 100%;
+    color: #000000;
+    margin-top: 55px;
+  `,
+  notes: css`
+    margin-top: 75px;
+  `
+};
 
-const sectionStyles = css`
-  margin-right: 34px;
-  margin-bottom: 34px;
-  margin-top: 34px;
-`;
-
-// HEADER PIECES
-const invoiceNumber = "#10000023456";
+// HEADER VALUES
+const invoiceNumber = "#1030090212";
 const campaignName = "FakeName";
 const lastEdited = "date";
 const currentStatus = "Draft";
 const labelsCount = 0;
-const addALabel = "";
+
+const invoiceNumberBlock = (
+  <React.Fragment>
+    <p css={headerStyles.invoice}>{invoiceNumber}</p>
+    <p css={headerStyles.subheading}>
+      {`${campaignName} Campaign`} | {`Last Edited ${lastEdited}`}
+    </p>
+  </React.Fragment>
+);
+
+const statusBlock = (
+  <div css={headerStyles.statusBlock}>
+    <p css={headerStyles.status}>Current Status</p>
+    <p css={headerStyles.actualStatus}>{currentStatus}</p>
+  </div>
+);
+
+const labelBlock = (
+  <div css={headerStyles.labelBlock}>
+    <p css={headerStyles.labels}>{`Labels (${labelsCount})`}</p>
+    <p css={headerStyles.smallBlueText}>+ Add Labels</p>
+  </div>
+);
 
 const AddContribution = () => (
   <AddContributionForm
@@ -183,38 +237,31 @@ const AddContribution = () => (
     }) => (
       <React.Fragment>
         {/* HEADER SECTION */}
-        <div css={headerStyles.header}>
+        <div css={containers.header}>
           <div css={headerStyles.leftColumn}>
-            <p css={headerStyles.invoice}>{invoiceNumber}</p>
-            <div css={headerStyles.subheading}>
-              <p>
-                {`${campaignName} Campaign`} | {`Last Edited ${lastEdited}`}
-              </p>
-            </div>
-            <p css={headerStyles.labels}>{`Labels (${labelsCount})`}</p>
-            <p css={headerStyles.addLabels}>{`+ Add Labels ${addALabel}`}</p>
+            {invoiceNumberBlock}
+            <div style={{ display: "flex" }}>{labelBlock}</div>
           </div>
           <div css={headerStyles.rightColumn}>
-            <div css={headerStyles.statusBlock}>
-              <p css={headerStyles.status}>Current Status</p>
-              <p css={headerStyles.actualStatus}>{currentStatus}</p>
+            <div style={{ flexDirection: "column" }}>
+              {statusBlock}
+              <Button
+                css={headerStyles.submitButton}
+                buttonType="submit"
+                disabled={!isValid}
+                onClick={handleSubmit}
+              >
+                Submit Contribution
+              </Button>
             </div>
-            <Button
-              css={headerStyles.submitButton}
-              buttonType="submit"
-              disabled={!isValid}
-              onClick={handleSubmit}
-            >
-              Submit Contribution
-            </Button>
           </div>
         </div>
-        <hr style={{ marginRight: "38px" }} />
+        <hr css={sectionStyles.dividerLine} />
 
         {/* BASICS SECTION */}
-        <div css={sectionStyles}>
-          <h3 css={sectionTitle}>Basics</h3>
-          <div css={container}>
+        <div css={sectionStyles.main}>
+          <h3 css={sectionStyles.title}>Basics</h3>
+          <div css={containers.main}>
             <h2>{formFields.dateOfContribution}</h2>
             <h2>{formFields.typeOfContribution}</h2>
             <h2>{formFields.subTypeOfContribution}</h2>
@@ -227,37 +274,37 @@ const AddContribution = () => (
         </div>
 
         {/* CONTRIBUTOR SECTION */}
-        <div css={sectionStyles}>
-          <h3 css={sectionTitle}>Contributor</h3>
-          <div css={contributorContainer}>
+        <div css={sectionStyles.main}>
+          <h3 css={sectionStyles.title}>Contributor</h3>
+          <div css={containers.sectionTwo}>
             <h2>{formFields.firstName}</h2>
             <h2>{formFields.lastName}</h2>
           </div>
-          <h2 css={addressContainer}>{formFields.streetAddress}</h2>
-          <h2 css={addressContainer}>{formFields.addressLine2}</h2>
-          <div css={cityStateZip}>
+          <h2 css={containers.fullWidth}>{formFields.streetAddress}</h2>
+          <h2 css={containers.fullWidth}>{formFields.addressLine2}</h2>
+          <div css={containers.cityStateZip}>
             <h2>{formFields.city}</h2>
             <h2>{formFields.state}</h2>
             <h2>{formFields.zipcode}</h2>
           </div>
-          <div css={contributorContainer}>
+          <div css={containers.sectionTwo}>
             <h2>{formFields.contactType}</h2>
             <h2>{formFields.contactInformation}</h2>
             <h2>{formFields.occupation}</h2>
             <h2>{formFields.employerName}</h2>
           </div>
-          <div css={cityStateZip}>
+          <div css={containers.cityStateZip}>
             <h2>{formFields.employerCity}</h2>
             <h2>{formFields.employerState}</h2>
             <h2>{formFields.employerZipcode}</h2>
-            <h2>{formFields.occupationLetterDate}</h2>
           </div>
+          <h2 css={containers.fullWidth}>{formFields.occupationLetterDate}</h2>
         </div>
 
         {/* OTHER DETAILS SECTION */}
-        <div css={sectionStyles}>
-          <h3 css={sectionTitle}>Other Details</h3>
-          <div css={container}>
+        <div css={sectionStyles.main}>
+          <h3 css={sectionStyles.title}>Other Details</h3>
+          <div css={containers.main}>
             <h2>{formFields.electionAggregate}</h2>
             <h2>{formFields.description}</h2>
             <h2>{formFields.linkToDocumentation}</h2>
