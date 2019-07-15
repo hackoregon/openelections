b4390c61ad25bb8200c113e5028f06423d322310
Jaron Heard <jaronheard@gmail.com>
2019-07-15 05:00:38 -0700

    refactor and connect sign-up to api

--- app/src/Pages/SignUp/SignUp.js
+++ app/src/Pages/SignUp/SignUp.js
@@ -1,57 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import queryString from "query-string";
+import { flashMessage } from "redux-flash";
 import PageHoc from "../../components/PageHoc/PageHoc";
-import FormModal from "../../components/FormModal/FormModal"
-import SignupForm from "../../components/Forms/Signup";
-import Button from "../../components/Button/Button"
-/** @jsx jsx */
-import { css, jsx } from "@emotion/core";
+import SignUpForm from "../../components/Forms/SignUp/index";
+import { connect } from "react-redux";
+import { redeemInvite, isLoggedIn } from "../../state/ducks/auth";
 
-const formTitle = css`
-  font-size: 35px;
-  letter-spacing: -2px;
-  margin: 10px 0px;
-`;
-const buttonWrapper = css`
-  margintop: 30px;
-`;
+class SignUp extends Component {
+  componentDidUpdate() {
+    const { isLoggedIn, authError, flashMessage, history } = this.props;
+    if (isLoggedIn) {
+      flashMessage("Signup Success", { props: { variant: "success" } }); // Todo, implement success
+      history.push("/dashboard");
+    } else if (authError) {
+      flashMessage("Signup Error", { props: { variant: "error" } }); //Todo, implement failure
+    }
+  }
 
-const SignUp = () => (
-	<PageHoc>
-		<FormModal>
-			<SignupForm
-				onSubmit={x => console.log("REPLACE ME WITH SOMETHING REAL!")}
-				initialValues={{
-					newPassword: "",
-					confirmNewPassword: ""
-				}}
-			>
-				{({
-					  formSections,
-					  isValid,
-					  handleSubmit /* isDirty, isSubmitting */
-				  }) => (
-					<React.Fragment>
-						<p css={formTitle}>Signup</p>
-						<p>
-							Create a strong password to complete the sign up process
-						</p>
-						{formSections.newPassword}
-						{formSections.confirmNewPassword}
-						<div css={buttonWrapper}>
-							<br/>
-							<Button
-								buttonType="submit"
-								disabled={!isValid}
-								onClick={handleSubmit}
-							>
-								Submit
-							</Button>
-						</div>
-					</React.Fragment>
-				)}
-			</SignupForm>
-		</FormModal>
-	</PageHoc>
-);
+  render() {
+    const { location } = this.props;
+    const params = queryString.parse(location.search);
+    const { invitationCode } = params;
+    return (
+      <PageHoc>
+        <SignUpForm code={invitationCode} {...this.props} />
+      </PageHoc>
+    );
+  }
+}
 
-export default SignUp;
+export default connect(
+  state => ({
+    isLoggedIn: isLoggedIn(state) || false,
+    authError: state.auth.error
+  }),
+  dispatch => {
+    return {
+      redeemInvite: (invitationCode, password) =>
+        dispatch(redeemInvite(invitationCode, password)),
+      flashMessage: (message, options) =>
+        dispatch(flashMessage(message, options)),
+      dispatch
+    };
+  }
+)(SignUp);
