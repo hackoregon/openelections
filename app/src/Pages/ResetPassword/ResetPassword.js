a20772734b7f3e332f6b5f7ac35ae1013c9aee66
Michael Scotto di Carlo <mscotto@supportland.com>
2019-07-15 19:07:16 -0700

    Now reset-password redirects to sign-in and logsout after sucessful submit

--- app/src/Pages/ResetPassword/ResetPassword.js
+++ app/src/Pages/ResetPassword/ResetPassword.js
@@ -2,9 +2,8 @@ import React from "react";
 import PageHoc from "../../components/PageHoc/PageHoc";
 import ResetPasswordForm from "../../components/Forms/ResetPassword/index";
 import { connect } from "react-redux";
-import { updatePassword, isLoggedIn } from "../../state/ducks/auth";
+import { updatePassword, isLoggedIn, logout } from "../../state/ducks/auth";
 import { flashMessage } from "redux-flash";
-import { isNull } from "util";
 
 class ResetPassword extends React.Component {
   constructor(props) {
@@ -15,13 +14,14 @@ class ResetPassword extends React.Component {
   }
 
   componentDidUpdate(){
-    const { error, flashMessage, history, isLoading } = this.props;
+    const { error, flashMessage, history, isLoading, logout } = this.props;
     if (this.state.submitted && !isLoading){
     if(error){
       flashMessage("Password update failed", {props:{variant:'error'}});
     }else {
+      logout();
       flashMessage("Password updated", {props:{variant:'success'}});
-      history.push('/dashboard');
+      history.push('/sign-in');
     }
   }
   }
@@ -48,7 +48,8 @@ export default connect(
   dispatch => {
     return {
       updatePassword: (oldPassword,newPassword) => dispatch(updatePassword(oldPassword,newPassword)),
-      flashMessage: (message, options) => dispatch(flashMessage(message, options)),    
+      flashMessage: (message, options) => dispatch(flashMessage(message, options)),
+      logout: () => dispatch(logout()), 
       dispatch
       }
     }
