6e870cb038a05558bb7ceb9e536f1f93914cc1c0
Kelly de Vries <kdv24@users.noreply.github.com>
2019-07-21 17:32:43 +0200

    Fix styling of remove user modal-  GH 442 (#458)
    * fix typo- mangage
    
    * make buttons the same size
    
    * make content fit within modal
    
    - set maxWidth of content to less than modal maxWidth
    - set word-break to alleviate overflow of long email addresses
    - removed unneeded flex styling that was causing squashing of button

--- app/src/components/Forms/RemoveUser/RemoveUser.js
+++ app/src/components/Forms/RemoveUser/RemoveUser.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import { connect } from "react-redux";
-import Button from '../../Button/Button'
+import Button from "../../../components/Button/Button";
 import { getModalState, clearModal } from "../../../state/ducks/modal";
 import FormModal from "../../FormModal/FormModal";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
 const removeUserStyle = css`
-  padding: 40px;
+  word-break: break-word;
+  max-width:300px;
 `;
 const buttonContainer = css`
   display: flex;
-  flex-direction: row;
   margin-top: 30px;
-  align-items: center;
-  justify-content: space-around;
   button {
     margin: 10px;
   }
+`;
+const removeUserTitle = css`
+  font-size: 36px;
 `;
 
 const RemoveUser = (props) => {
@@ -25,14 +26,14 @@ const RemoveUser = (props) => {
   return (
     <FormModal>
     <div css={removeUserStyle}>
+      <h1 css={removeUserTitle}>Remove User </h1>
       <p>{props.location.state.email} will no longer have access to the portal.</p>
       <p>Are you sure you want to remove them?</p>
       <div css={buttonContainer}>
         <Button buttonType="formDefaultOutlined" onClick={() => props.clearModal()}>
           Cancel
             </Button>
-        <Button
-          buttonType="formDefault" onClick={
+        <Button buttonType="formDefault" onClick={
             () => props.removeUser(props.location.state.id, props.location.state.roleId)
             }>
           Submit
