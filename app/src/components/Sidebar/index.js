8f2bb16e0852bc4809422a2749cae140b516b324
Belal Sejouk <billoosijok@gmail.com>
2019-07-21 22:58:12 -0700

    Display 'City Portal' instead of Campaign Name on Sidebar for gov admin

--- app/src/components/Sidebar/index.js
+++ app/src/components/Sidebar/index.js
@@ -3,5 +3,6 @@ import Sidebar from "./Sidebar";
 import { getCampaignName } from "../../state/ducks/campaigns";
 
 export default connect(state => ({
-  campaignName: getCampaignName(state)
+  campaignName: getCampaignName(state),
+  governmentId: state.governments.currentGovernmentId
 }))(Sidebar);
