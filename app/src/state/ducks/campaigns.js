7300d7ac4709266bb31a5eb2bc68613bd8736ede
iankerriganharris <iankerriganharris@gmail.com>
2019-07-22 20:23:17 -0700

    Add a thunk to list campaigns by government id (#487)

--- app/src/state/ducks/campaigns.js
+++ app/src/state/ducks/campaigns.js
@@ -11,7 +11,8 @@ export const STATE_KEY = "campaigns";
 // Action Types
 export const actionTypes = {
   CREATE_CAMPAIGN: createActionTypes(STATE_KEY, "CREATE_CAMPAIGN"),
-  SET_CAMPAIGN: createActionTypes(STATE_KEY, "SET_CAMPAIGN")
+  SET_CAMPAIGN: createActionTypes(STATE_KEY, "SET_CAMPAIGN"),
+  GET_CAMPAIGNS: createActionTypes(STATE_KEY, "GET_CAMPAIGNS")
 };
 
 // Initial State
@@ -37,6 +38,15 @@ export default createReducer(initialState, {
   },
   [actionTypes.SET_CAMPAIGN.SUCCESS]: (state, action) => {
     return { ...state, currentCampaignId: action.campaignId };
+  },
+  [actionTypes.GET_CAMPAIGNS.REQUEST]: (state, action) => {
+    return { ...state, isLoading: true };
+  },
+  [actionTypes.GET_CAMPAIGNS.SUCCESS]: (state, action) => {
+    return { ...state, isLoading: false };
+  },
+  [actionTypes.GET_CAMPAIGNS.FAILURE]: (state, action) => {
+    return { ...state, isLoading: false, error: action.error };
   }
 });
 
@@ -48,7 +58,13 @@ export const actionCreators = {
     failure: error => action(actionTypes.CREATE_CAMPAIGN.FAILURE, { error })
   },
   setCampaign: {
-    success: campaignId => action(actionTypes.SET_CAMPAIGN.SUCCESS, { campaignId }),
+    success: campaignId =>
+      action(actionTypes.SET_CAMPAIGN.SUCCESS, { campaignId })
+  },
+  getCampaigns: {
+    request: () => action(actionTypes.GET_CAMPAIGNS.REQUEST),
+    success: () => action(actionTypes.GET_CAMPAIGNS.SUCCESS),
+    failure: error => action(actionTypes.GET_CAMPAIGNS.FAILURE, { error })
   }
 };
 
@@ -57,10 +73,7 @@ export function createCampaignForGovernment(campaignAttrs) {
   return async (dispatch, getState, { api, schema }) => {
     dispatch(actionCreators.createCampaign.request());
     try {
-      const response = await api.createCampaignForGovernment(
-        campaignAttrs
-      );
-      console.log('eys')
+      const response = await api.createCampaignForGovernment(campaignAttrs);
       if (response.status === 201) {
         const data = normalize(await response.json(), schema.campaign);
         dispatch(addEntities(data.entities));
@@ -70,6 +83,24 @@ export function createCampaignForGovernment(campaignAttrs) {
       }
     } catch (error) {
       dispatch(actionCreators.createCampaign.failure(error));
+    }
+  };
+}
+
+export function getCampaigns(governmentId) {
+  return async (dispatch, getState, { api, schema }) => {
+    dispatch(actionCreators.getCampaigns.request());
+    try {
+      const response = await api.getCampaignsForGovernment(governmentId);
+      if (Array.isArray(response)) {
+        const data = normalize(response, schema.campaign);
+        dispatch(addEntities(data.entities));
+        dispatch(actionCreators.getCampaigns.success());
+      } else {
+        dispatch(actionCreators.getCampaigns.failure());
+      }
+    } catch (error) {
+      dispatch(actionCreators.getCampaigns.failure(error));
     }
   };
 }
@@ -84,7 +115,7 @@ export const getCampaignInfo = createSelector(
 
 //Assumes one campaign
 export const getCampaignName = state => {
-  const id = (getCampaignInfo(state).currentCampaignId)
+  const id = getCampaignInfo(state).currentCampaignId
     ? getCampaignInfo(state).currentCampaignId
     : 0;
   return getCampaignInfo(state)[id]
