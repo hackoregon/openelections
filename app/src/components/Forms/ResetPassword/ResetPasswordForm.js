5ffdcf95ff707a6e18bb69157ecf0aad65983b9b
Jaron Heard <jaronheard@gmail.com>
2019-07-22 22:08:13 -0700

    change reset password fields to password fields

--- app/src/components/Forms/ResetPassword/ResetPasswordForm.js
+++ app/src/components/Forms/ResetPassword/ResetPasswordForm.js
@@ -2,20 +2,20 @@ import React from "react";
 import * as Yup from "yup";
 
 import Form from "../../Form/Form";
-import TextField from "../../Fields/TextField";
+import PasswordField from "../../Fields/PasswordField";
 
 const fields = {
   oldPassword: {
     label: "Current Password",
     section: "resetPassword",
-    component: TextField,
+    component: PasswordField,
     validation: Yup.string("Enter your current password")
         .required("Current password is required")
   },
   newPassword: {
     label: "New Password",
     section: "resetPassword",
-    component: TextField,
+    component: PasswordField,
     validation: Yup.string("Enter your new password")
         .required("New password is required")
         .min(6, "Passwords must be 6 characters")
