2704146d768391c95f49dbb8c7f663cefe251364
Jason Valenzuela <jason.valenzuela@hackoregon.org>
2019-07-26 10:27:48 -0700

    adding test to contributions table

---
+++ app/src/Pages/Portal/Contributions/ContributionsTable/ContributionsTable.test.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { shallow } from "enzyme"
+import ContributionsTable from "./ContributionsTable"
+
+describe("<ContributionsTable/>", () => {
+    it("should be defined", () => {
+        const wrapper = shallow(<ContributionsTable/>);
+        expect(wrapper).toBeDefined();
+        expect(wrapper).toMatchSnapshot();
+    })
+})
