8d9b952d91ff43d14a1e11573736328fc3776a7a
Kelly de Vries <kdv@me.com>
2019-07-26 11:01:11 +0200

    update contribution/expense naming in routes for accuracy

--- app/src/Pages/routes.js
+++ app/src/Pages/routes.js
@@ -10,8 +10,8 @@ import SignInPage from "./SignIn/SignIn";
 import SignUpPage from "./SignUp/SignUp";
 import ForgotPasswordPage from "./ForgotPassword/ForgotPassword";
 import UpdateForgottenPasswordPage from "./UpdateForgottenPassword/UpdateForgottenPassword";
-import AddContributionPage from "./Portal/Contributions/Contributions";
-import AddExpensePage from "./Portal/Expenses/Expenses";
+import ContributionPage from "./Portal/Contributions/Contributions";
+import ExpensePage from "./Portal/Expenses/Expenses";
 import Portal from "./Portal/Portal";
 import ResetPassword from "./ResetPassword/ResetPassword";
 
@@ -46,10 +46,10 @@ const Routes = props => {
                 <Route exact path="/sign-up" component={SignUpPage} />
                 <Route
                   exact
-                  path="/add-contribution"
-                  component={AddContributionPage}
+                  path="/contributions"
+                  component={ContributionPage}
                 />
-                <Route exact path="/add-expense" component={AddExpensePage} />
+                <Route exact path="/expenses" component={ExpensePage} />
                   path="/reset-password"
                   component={ResetPassword}
                 />
