556c1b0e8d64c8700493f8ce9a66ca1634f50f2f
Belal Sejouk <billoosijok@gmail.com>
2019-07-31 18:52:13 -0700

    Dashboard edits (#517)
    * GH-483 WIP: Redesigning dashboard cards
    
    * Phase 1 of dashboard re-design
    
    * Setup contributions card to get data from the server in the future
    
    * Add new links to dashboard
    
    * Fix issue with LoadingCircle file
    
    * Improve search box component

---
+++ app/src/Pages/Portal/Dashboard/cards/SearchCard.js
@@ -0,0 +1,72 @@
+import * as React from "react";
+
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { NavLink } from "react-router-dom";
+import Form from "../../../../components/Form/Form";
+import SearchBox from "../../../../components/SearchBox/SearchBox";
+
+const styles = css`
+	.search-box {
+		font-size: 16px;	
+	}
+		
+`;
+
+class SearchCard extends React.Component {
+
+	queryTimeout = null;
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isLoading: false,
+			searchResults: null
+		}
+	}
+
+	performSearch (query) {
+		if ( !this.state.isLoading ) {
+			this.setState({isLoading: true});
+		}
+
+		// Pretend to search
+		setTimeout(() => {
+			this.setState({
+				isLoading: false,
+				searchResults: [
+					{ label: 'Expense', value: 'Example' },
+					{ label: 'Contribution', value: 'Example contribution' },
+					{ label: 'Other', value: 'Other type of content' }
+				]
+			});
+		}, 2000)
+	}
+
+	render() {
+		return (
+			<div css={styles}>
+				<h3>Search</h3>
+				<p>Search across all contributions and expenses</p>
+				<SearchBox
+					className='search-box'
+					placeholder='Contribution #, Expenses #'
+					onSearchQueryChange={(value) => {
+						if (value) {
+							if (this.queryTimeout) clearTimeout(this.queryTimeout);
+
+							this.queryTimeout = setTimeout( () => {
+								this.performSearch(value);
+								this.queryTimeout = null;
+							}, 1000);
+						}
+					}}
+					isLoading={this.state.isLoading}
+					guesses={this.state.searchResults}
+				/>
+			</div>
+		);
+	}
+};
+export default SearchCard;
