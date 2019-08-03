7c6ba9505f2c6549367801f2fe90f43230905404
Dan Melton <melton.dan@gmail.com>
2019-08-03 10:12:09 -0700

    fixes add user problem closes #542 529

--- app/src/components/Invitation/index.js
+++ app/src/components/Invitation/index.js
@@ -5,7 +5,7 @@ import FormModal from "../FormModal/FormModal";
 import Button from "../Button/Button";
 
 const formTitle = css`
-  font-size: 35px;
+  font-size: 24px;
   letter-spacing: -2px;
   margin: 10px 0px;
 `;
@@ -21,17 +21,15 @@ const Invitation = ({ campaign, government, code /* email */ }) => (
   <FormModal>
     <div css={center}>
     <p>
-      You've been invited to join the following portal on Open Election by
-      Civic:
+      You've been invited to join:
     </p>
     <p css={formTitle}>
-      {`${campaign}${government} ${campaign && campaign.length > 0 ? "Campaign" : ""}${
-        government && government.length > 0 ? "Government" : ""
+      {`${campaign && campaign.length > 0 ? campaign : ""}${
+        government && government.length > 0 ? government : ""
       } Portal`}
     </p>
     <p>
-      To complete your invitation, click the Sign up button in this email and
-      follow the instructions provided.
+      in the Open and Accountable Elections Portland program. Click below to complete your registration.
     </p>
     <div css={buttonWrapper}>
       <Link to={`/sign-up?invitationCode=${code}`}>
