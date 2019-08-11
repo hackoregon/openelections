3bfe08ee5a0bd636bec7131755ad71801e62d60c
Ian Harris <iankerriganharris@gmail.com>
2019-08-11 16:39:44 -0700

    Add process call to entrypoint file

--- api/jobs/queues/dataScienceResultQueue.ts
+++ api/jobs/queues/dataScienceResultQueue.ts
@@ -1,10 +1,7 @@
 import * as Bull from 'bull';
 
 import config from './config';
-import { dataScienceRetrieverProcessor } from '../processors';
 
 const dataScienceResultQueue = new Bull('ds-results-retriever', config.redis);
-
-dataScienceResultQueue.process(config.concurrency, dataScienceRetrieverProcessor);
 
 export default dataScienceResultQueue;
