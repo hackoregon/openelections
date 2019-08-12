802fa63ba88d539971723582b6997891f9c702c1
Belal Sejouk <billoosijok@gmail.com>
2019-08-11 19:34:37 -0700

    Bug fixes

--- app/src/components/Fields/DateRangeField.test.js
+++ app/src/components/Fields/DateRangeField.test.js
@@ -3,10 +3,6 @@ import DateRangeField from "./DateRangeField";
 
 describe("<DateRangeield/>", () => {
 	it("should be defined", () => {
-<<<<<<< HEAD
 		expect(DateRangeField).toMatchSnapshot();
-=======
-		expect(DateRangeield).toMatchSnapshot();
->>>>>>> Created DateRange field and added to the contributions filter
 	});
 });
