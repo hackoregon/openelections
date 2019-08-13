acd69eb9496844fc67b80d0d5b987d5838383ce0
Ian Harris <iankerriganharris@gmail.com>
2019-08-12 21:08:36 -0700

    Fix path to processor files

--- api/jobs/processors/index.ts
+++ api/jobs/processors/index.ts
@@ -1,3 +1,6 @@
 import * as path from 'path';
 
-export const dataScienceRetrieverProcessor = path.resolve('jobs/processors/build/dataScienceRetrieverProcessor.js');
+export const dataScienceRetrieverProcessor =
+    process.env.NODE_ENV === 'development'
+        ? path.resolve('jobs/processors/build/dataScienceRetrieverProcessor.js')
+        : path.resolve('build/jobs/processors/dataScienceRetrieverProcessor.js');
