b4bdc562d79c9401f891c27a8f417415fb532b72
Kelly de Vries <kdv@me.com>
2019-08-13 19:07:12 -0700

    add initial phone field validation masking- same issues as currency & email

--- app/src/components/Fields/PhoneField.js
+++ app/src/components/Fields/PhoneField.js
@@ -1,27 +1,56 @@
 import React from "react";
 import PropTypes from "prop-types";
+import TextFieldMaterial from "@material-ui/core/TextField";
 import MaskedInput from 'react-text-mask'
 
-const PhoneField = ({ id, label, formik }) => (
-  <MaskedInput
-    id={id}
-    name={id}
-    label={label}
-    helperText={formik.touched[id] ? formik.errors[id] : ""}
-    error={formik.touched[id] && Boolean(formik.errors[id])}
-    value={formik.values[id]}
-    onChange={formik.handleChange}
-    onBlur={formik.handleBlur}
-    autoComplete="on"
-    fullWidth
-    mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
-/>
-);
+function TextMaskCustom(props) {
+    const { inputRef, ...other } = props;
+  
+    return (
+      <MaskedInput
+        {...other}
+        ref={ref => {
+          inputRef(ref ? ref.inputElement : null);
+        }}
+        mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+      />
+    );
+  }
+
+export default function PhoneField({ id, label, formik }) {
+    const [values, setValues] = React.useState({
+        textmask: formik.values[id],
+
+      });
+
+      const handleChange = name => event => {
+        setValues({
+          ...values,
+          [name]: event.target.value,
+        });
+      };
+
+      return(
+        <TextFieldMaterial
+            id={id}
+            name={id}
+            label={label}
+            helperText={formik.touched[id] ? formik.errors[id] : ""}
+            error={formik.touched[id] && Boolean(formik.errors[id])}
+            value={values.textmask}
+            onChange={handleChange('textmask')}
+            onBlur={formik.handleBlur}
+            autoComplete="on"
+            fullWidth
+            InputProps={{
+                inputComponent: TextMaskCustom,
+            }}
+        />
+    )
+}
 
 PhoneField.propTypes = {
   id: PropTypes.string,
   label: PropTypes.string,
   formik: PropTypes.shape({})
 };
-
-export default PhoneField;
