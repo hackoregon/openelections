f90fcf55b4bcebaf4899c3608c23766552a05161
Dan Melton <melton.dan@gmail.com>
2019-08-17 21:42:54 -0700

    adds id to params type

--- api/routes/helpers.ts
+++ api/routes/helpers.ts
@@ -3,6 +3,7 @@ import { Request } from 'express';
 
 export interface IRequest extends Request {
     currentUser?: IToken;
+    id?: string;
 }
 
 export async function getCurrentUser(req: IRequest, res, next) {
