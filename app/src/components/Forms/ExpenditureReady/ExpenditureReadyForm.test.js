768df6f3debffd951f7ca1ce3b305a9615377e04
Kelly de Vries <kdv@me.com>
2019-08-28 11:07:05 -0700

    remove unused forms

--- app/src/components/Forms/ExpenditureReady/ExpenditureReadyForm.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ExpenditureReadyForm from './ExpenditureReadyForm';
-
-describe('<ExpenditureReadyForm/>', () => {
-  it('should be defined', () => {
-    expect(ExpenditureReadyForm).toMatchSnapshot();
-  });
-});
