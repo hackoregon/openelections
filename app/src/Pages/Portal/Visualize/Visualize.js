87d36f55d5bdaf030cd7671e7e23052e006e3656
Michael Lange <dingoeatingfuzz@gmail.com>
2019-09-09 01:20:07 -0700

    Implement the three data visualizations, including a map

--- app/src/Pages/Portal/Visualize/Visualize.js
+++ app/src/Pages/Portal/Visualize/Visualize.js
@@ -1,10 +1,18 @@
 import * as React from "react";
 import PageHoc from "../../../components/PageHoc/PageHoc";
 import ContributionsData from "./mock-contributions.json";
+import { civicFormat } from "@hackoregon/component-library/dist/utils";
+import {
+  HorizontalBarChart,
+  BaseMap,
+  ScreenGridMap
+} from "@hackoregon/component-library";
 
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 import { mediaQueryRanges, accents } from "../../../assets/styles/variables";
+
+const { numeric, dollars } = civicFormat;
 
 const styles = css`
   .cards-wrapper {
@@ -13,7 +21,6 @@ const styles = css`
     .card {
       border: 2px solid ${accents.lightGrey};
       padding: 15px;
-      /* background: rgba(0,0,0,0.25); */
       margin-bottom: 15px;
 
       + .card {
@@ -27,6 +34,11 @@ const styles = css`
     }
   }
 
+  .big-number {
+    font-weight: bold;
+    font-size: 32px;
+  }
+
   @media ${mediaQueryRanges.mediumAndUp} {
     .cards-wrapper {
       display: flex;
@@ -34,18 +46,14 @@ const styles = css`
       flex-direction: row;
       width: 100%;
 
-      .card {
-        display: flex;
-      }
-
       .cards-ribbon {
-        height: 25vh;
+        height: 35vh;
         width: 100%;
         display: flex;
         flex-direction: row;
 
         .card {
-          width: 33.33%;
+          width: 50%;
         }
       }
 
@@ -57,7 +65,58 @@ const styles = css`
   }
 `;
 
+const titleCase = str =>
+  str
+    .split(/[_-]/)
+    .map(word => word[0].toUpperCase() + word.substr(1))
+    .join(" ");
+
+const summarizeBy = (data, key) =>
+  data.reduce((hash, d) => {
+    if (hash[d[key]]) {
+      hash[d[key]] += d.amount;
+    } else {
+      hash[d[key]] = d.amount;
+    }
+    return hash;
+  }, {});
+
+const arrFromHash = (hash, key = "key", value = "value") =>
+  Object.keys(hash).map(entry => ({
+    [key]: titleCase(entry),
+    [value]: hash[entry]
+  }));
+
+const uniqueDonorsCount = data => {
+  var s = new Set();
+  data.forEach(d => s.add(d.matchId || d.contributorName));
+  return s.size;
+};
+
+const donationsByContributorType = data =>
+  arrFromHash(summarizeBy(data, "contributionSubType"), "type", "amount");
+
+const donationsByContributionType = data =>
+  arrFromHash(summarizeBy(data, "contributorType"), "type", "amount");
+
+const propertiesFrom = data => data.features.map(f => f.properties);
+
+const OceanColorScheme = [
+  [237, 248, 177],
+  [199, 233, 180],
+  [127, 205, 187],
+  [65, 182, 196],
+  [29, 145, 192],
+  [34, 94, 168],
+  [37, 52, 148],
+  [8, 29, 88]
+];
+
 const VisualizePage = props => {
+  const table = propertiesFrom(ContributionsData);
+  const uniqueDonors = uniqueDonorsCount(table);
+  const byContributorType = donationsByContributorType(table);
+  const byContributionType = donationsByContributionType(table);
   return (
     <PageHoc>
       <div css={styles}>
@@ -65,17 +124,38 @@ const VisualizePage = props => {
         <div className="cards-wrapper">
           <div className="cards-ribbon">
             <div className="card">
-              <h3>Donors for this time period</h3>
-            </div>
-            <div className="card">
               <h3>Who is Donating?</h3>
+              <HorizontalBarChart
+                data={byContributorType}
+                dataValue="amount"
+                dataLabel="type"
+                xLabel="Dollars"
+                dataValueFormatter={dollars}
+                minimalist
+              />
             </div>
             <div className="card">
               <h3>How are they Donating?</h3>
+              <HorizontalBarChart
+                data={byContributionType}
+                dataValue="amount"
+                dataLabel="type"
+                xLabel="Dollars"
+                dataValueFormatter={dollars}
+                minimalist
+              />
             </div>
           </div>
           <div className="card featured">
-            <h3>Where donations are coming from</h3>
+            <BaseMap updateViewport={false} initialZoom={11} useContainerHeight>
+              <ScreenGridMap
+                data={ContributionsData.features}
+                getPosition={f => f.geometry.coordinates}
+                opacity={0.8}
+                colorRange={OceanColorScheme}
+                cellSizePixels={50}
+              />
+            </BaseMap>
           </div>
         </div>
       </div>
