add90ba5e147f07ab5ccd15a1d489d76b81f8db3
Dan Melton <melton.dan@gmail.com>
2019-09-10 08:57:46 -0700

    don't run jobs in test mode

--- api/jobs/helpers/addJobs.ts
+++ api/jobs/helpers/addJobs.ts
@@ -2,11 +2,17 @@ import { dataScienceResultQueue } from '../queues';
 import { gisQueue } from '../queues';
 
 export async function addDataScienceJob(jobData: {id: number}) {
+    if (process.env.NODE_ENV === 'test') {
+        return;
+    }
     await dataScienceResultQueue.add(jobData);
     return;
 }
 
 export async function addGisJob(jobData: {id: number}) {
+    if (process.env.NODE_ENV === 'test') {
+        return;
+    }
     await gisQueue.add(jobData);
     return;
 }
