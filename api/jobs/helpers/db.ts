8ce2e88e3a82d76610bf4f997a5bc76d6374001d
Dan Melton <melton.dan@gmail.com>
2019-09-10 18:44:44 -0700

    fixes issue with db connection for jobs

--- api/jobs/helpers/db.ts
+++ api/jobs/helpers/db.ts
@@ -1,20 +1,25 @@
 import 'reflect-metadata';
-import { createConnection, Connection } from 'typeorm';
+import { createConnection, Connection, getConnection } from 'typeorm';
 
 export default async (): Promise<Connection> => {
-    const connection: Connection = await createConnection({
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        entities: [
-            '/app/build/models/entity/*.js'
-        ],
-        synchronize: false,
-        logging: false
-    });
+    try {
+        const connection: Connection = await createConnection({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT),
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [
+                '/app/build/models/entity/*.js'
+            ],
+            synchronize: false,
+            logging: false
+        });
 
-    return connection;
+        return connection;
+    } catch (e) {
+        return getConnection('default');
+    }
+
 };
