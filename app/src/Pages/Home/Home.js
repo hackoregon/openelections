b633cb11983fd47a2aaad7d9e03ced3786f58fd1
Belal Sejouk <belal.sejouk@nike.com>
2019-09-10 20:22:48 -0700

    Add iframe

--- app/src/Pages/Home/Home.js
+++ app/src/Pages/Home/Home.js
@@ -1,11 +1,32 @@
 import * as React from 'react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 import PageHoc from '../../components/PageHoc/PageHoc';
-// import { connect } from 'react-redux'
+
+const slideShowStyles = css`
+  iframe {
+    display: block;
+    margin: auto;
+    max-width: 100%;
+    margin-top: 4vw;
+  }
+`;
 
 const HomePage = props => {
   return (
     <PageHoc>
-      <h1>Home</h1>
+      <div css={slideShowStyles} className="slideshow-wrapper">
+        <iframe
+          title="Demo Slideshow"
+          src="https://docs.google.com/presentation/d/e/2PACX-1vRj0QE82D4Iml7BCBaZxdo37pAZOnDyHqCTmFzFO7091GhuTOpOpSj0SU9GqDYewZlU5uL6uqZzbQ7K/embed?start=true&loop=true&delayms=5000"
+          frameBorder="0"
+          allowFullScreen="true"
+          width="960"
+          height="569"
+          mozallowfullscreen="true"
+          webkitallowfullscreen="true"
+        />
+      </div>
     </PageHoc>
   );
 };
