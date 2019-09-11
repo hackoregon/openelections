fd70eb5e067ff6ef56de2ab3fb0b4c00ad00fb80
Dan Melton <melton.dan@gmail.com>
2019-09-11 10:18:57 -0700

    fix bugsnag issue

--- api/services/bugsnagService.ts
+++ api/services/bugsnagService.ts
@@ -1,4 +1,9 @@
 import bugsnag from '@bugsnag/js';
 
-export const bugsnagClient = bugsnag(process.env.BUG_SNAG_API_KEY);
+export const bugsnagClient = (() => {
+    if (process.env.NODE_ENV === 'productiyon') {
+        return bugsnag(process.env.BUG_SNAG_API_KEY);
+    }
+    return;
+})();
 
