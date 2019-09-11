d2c3f14a9f4c159fa6a15b085eed10643dd8c392
Dan Melton <melton.dan@gmail.com>
2019-09-11 10:19:15 -0700

    fix typo

--- api/services/bugsnagService.ts
+++ api/services/bugsnagService.ts
@@ -1,7 +1,7 @@
 import bugsnag from '@bugsnag/js';
 
 export const bugsnagClient = (() => {
-    if (process.env.NODE_ENV === 'productiyon') {
+    if (process.env.NODE_ENV === 'production') {
         return bugsnag(process.env.BUG_SNAG_API_KEY);
     }
     return;
