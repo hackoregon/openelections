8c38d650ee84bc47d78e3ee2f0b4e486b611a31e
Dan Melton <melton.dan@gmail.com>
2019-09-15 10:39:09 -0700

    adds the ability to requeue all ds jobs

---
+++ api/jobs/queues/requeueAllDs.ts
@@ -0,0 +1,19 @@
+import db from '../../models/db';
+import { getConnection } from 'typeorm';
+import { Contribution } from '../../models/entity/Contribution';
+import { dataScienceResultQueue } from './index';
+
+export async function requeueDataScienceJobs() {
+    await db();
+    const rep = getConnection().getRepository('Contribution');
+    const contr = await rep.find();
+    const promises = [];
+    contr.forEach( (contr: Contribution) => {
+        dataScienceResultQueue.add({id: contr.id});
+        console.log(`Queued Datascience job for Contribution ${contr.id}`);
+    });
+    await Promise.all(promises);
+    return;
+}
+
+// requeueDataScienceJobs();
