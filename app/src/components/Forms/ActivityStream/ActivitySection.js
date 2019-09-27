94e99d3d40d63c7fedc5b3b5fca1380b8d02fe36
Kelly de Vries <kdv@me.com>
2019-09-27 08:17:28 -0700

    make records only show relevant activity history

--- app/src/components/Forms/ActivityStream/ActivitySection.js
+++ app/src/components/Forms/ActivityStream/ActivitySection.js
@@ -4,11 +4,27 @@ import { jsx } from '@emotion/core';
 import MessageBox from './MessageBox';
 import { activitySectionStyles } from '../../../assets/styles/forms.styles';
 
-export const ActivityList = ({ activitiesArray }) => {
-  return Object.values(activitiesArray).map((activity, index) => {
-    const capitalizedActivity =
-      activity.activityType.charAt(0).toUpperCase() +
-      activity.activityType.slice(1);
+export const ActivityList = ({
+  activitiesArray,
+  contributionId,
+  expenditureId,
+  currentId = contributionId || expenditureId,
+  filteredArray = [],
+}) => {
+  activitiesArray.forEach(activity => {
+    // How to do more efficiently? KELLY
+    if (activity.activityId === parseInt(currentId)) {
+      console.log('matches: ', currentId, activity.activityId);
+      filteredArray.push(activity);
+      // }
+      console.log({ filteredArray });
+    }
+  });
+
+  return Object.values(filteredArray).map((activity, index) => {
+    // const capitalizedActivity =
+    //   activity.activityType.charAt(0).toUpperCase() +
+    //   activity.activityType.slice(1);
     return (
       <div key={index}>
         <div css={activitySectionStyles.timelineGroup}>
@@ -38,7 +54,11 @@ export const ActivitySection = ({
     <hr css={activitySectionStyles.divider} />
     <h3 css={activitySectionStyles.title}>Transaction History</h3>
     <ul css={activitySectionStyles.activityList}>
-      <ActivityList activitiesArray={activitiesArray} />
+      <ActivityList
+        activitiesArray={activitiesArray}
+        contributionId={contributionId}
+        expenditureId={expenditureId}
+      />
     </ul>
     <MessageBox
       contributionId={contributionId}
