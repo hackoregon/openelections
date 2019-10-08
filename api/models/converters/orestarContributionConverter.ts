9664f50b80e4195609ae6984103c10ade01ced6d
Andrew <andyerickson2014@gmail.com>
2019-10-08 16:59:42 -0700

    outline project

---
+++ api/models/converters/orestarContributionConverter.ts
@@ -0,0 +1,27 @@
+import { Contribution } from "../entity/Contribution";
+
+export default class OrestarContributionConverter {
+
+  private contribution: Contribution;
+
+  constructor(contribution: Contribution) {
+    this.contribution = contribution;
+  }
+
+  public generate() {
+    return `<?xml version="1.0"?><address>
+    <street1>5903 SW Corbett Ave Apt 5</street1>
+    <street2/>
+    <city>Portland</city>
+    <state>OR</state>
+    <zip>97239</zip>
+    </address>`;
+  }
+
+  public address() {}
+
+  public zip() {
+    return `<zip>97239</zip>`;
+  }
+
+}
---
+++ api/test/converters/orestarContributionsConverter.spec.ts
@@ -0,0 +1,62 @@
+import {expect} from 'chai';
+import {getConnection} from 'typeorm';
+import * as libxml from 'libxmljs';
+import { addressSchema, zipSchema } from '../schemas/schemas';
+import {newCampaignAsync, newContributionAsync, newGovernmentAsync, truncateAll} from '../factories';
+import {Government} from '../../models/entity/Government';
+import {Campaign} from '../../models/entity/Campaign';
+import OrestarContributionConverter from '../../models/converters/orestarContributionConverter';
+
+let repository: any;
+let government: Government;
+let campaign: Campaign;
+
+describe('orestar contribution converter', () => {
+
+  before(() => {
+    repository = getConnection('default').getRepository('Contribution');
+  });
+
+  beforeEach(async () => {
+    government = await newGovernmentAsync();
+    campaign = await newCampaignAsync(government);
+  });
+
+  afterEach(async () => {
+    await truncateAll();
+  });
+
+  // it('tests', async () => {
+
+  //   const contribution = await newContributionAsync(campaign, government);
+
+  //   const xml = new OrestarContributionConverter(contribution);
+
+  //   const xsd = addressSchema;
+  //   const xml_valid = xml.generate();
+
+  //   var xsdDoc = libxml.parseXml(xsd);
+  //   var xmlDocValid = libxml.parseXml(xml_valid);
+  //   xmlDocValid.validate(xsdDoc)
+  //   console.log(xmlDocValid.validationErrors);
+  //   expect(xmlDocValid.validate(xsdDoc)).to.equal(true);
+
+  // });
+
+  it('test', async () => {
+    const contribution = await newContributionAsync(campaign, government);
+
+    const xml = new OrestarContributionConverter(contribution);
+
+    const xsd = zipSchema;
+    const xml_valid = xml.zip();
+
+    var xsdDoc = libxml.parseXml(xsd);
+    var xmlDocValid = libxml.parseXml(xml_valid);
+    xmlDocValid.validate(xsdDoc)
+    console.log(xmlDocValid.validationErrors);
+    expect(xmlDocValid.validate(xsdDoc)).to.equal(true);
+  });
+
+
+});
