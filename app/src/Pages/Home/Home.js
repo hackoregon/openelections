0097657e4a072ae47b2d3c3407aa91396208f61b
Michael Lange <dingoeatingfuzz@gmail.com>
2019-12-08 16:13:32 -0800

    Beginning of dataviz selectors
    Including public data, filter values, filter selections, and filtered
    public data.

--- app/src/Pages/Home/Home.js
+++ app/src/Pages/Home/Home.js
@@ -2,28 +2,70 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import PageHoc from '../../components/PageHoc/PageHoc';
-import { getPublicData } from '../../state/ducks/publicData';
+import {
+  getPublicData,
+  publicDataRequest,
+  allOffices,
+  allCampaigns,
+} from '../../state/ducks/publicData';
 
 class HomePage extends React.Component {
   componentDidMount() {
-    this.props.fetchPublicData();
+    const { fetchPublicData } = this.props;
+    fetchPublicData();
   }
 
   render() {
+    const { request, allOffices, allCampaigns } = this.props;
+    const { isLoading, error, data } = request;
+    console.log('GOT IT: ', isLoading, error, data);
     return (
       <PageHoc>
         <h1>Home</h1>
+        {isLoading && <em>Loading...</em>}
+        {error && <strong>Oh no! {error}</strong>}
+        {data && data.features && (
+          <p>Data Loaded! {data.features.length} records.</p>
+        )}
+        <h2>Offices</h2>
+        <ol>
+          {allOffices.map(office => (
+            <li key={office}>{office}</li>
+          ))}
+        </ol>
+        <h2>Campaigns</h2>
+        <ol>
+          {allCampaigns.map(campaign => (
+            <li key={campaign.id}>{campaign.name}</li>
+          ))}
+        </ol>
       </PageHoc>
     );
   }
 }
 
 HomePage.propTypes = {
-  fetchPublicData: PropTypes.func, // eslint-disable-line
+  fetchPublicData: PropTypes.func,
+  request: PropTypes.shape({
+    isLoading: PropTypes.bool,
+    error: PropTypes.string,
+    data: PropTypes.object,
+  }),
+  allOffices: PropTypes.arrayOf(PropTypes.string),
+  allCampaigns: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+    })
+  ),
 };
 
 export default connect(
-  state => ({}),
+  state => ({
+    request: publicDataRequest(state),
+    allOffices: allOffices(state),
+    allCampaigns: allCampaigns(state),
+  }),
   dispatch => {
     return {
       fetchPublicData: () => dispatch(getPublicData()),
--- app/src/state/ducks/publicData.js
+++ app/src/state/ducks/publicData.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import { createSelector } from 'reselect';
 import createReducer from '../utils/createReducer';
 import createActionTypes from '../utils/createActionTypes';
 import action from '../utils/action';
@@ -14,6 +15,10 @@ export const actionTypes = {
 // Initial State
 export const initialState = {
   data: {},
+  filters: {
+    campaigns: [],
+    offices: [],
+  },
   isLoading: false,
   error: null,
 };
@@ -56,3 +61,105 @@ export function getPublicData() {
     }
   };
 }
+
+export const rootState = state => state || {};
+export const publicData = createSelector(
+  rootState,
+  state => state[STATE_KEY] || {}
+);
+
+export const publicDataRequest = createSelector(
+  publicData,
+  state => {
+    const { data, isLoading, error } = state;
+    return { data, isLoading, error };
+  }
+);
+
+export const publicDataFilters = createSelector(
+  publicData,
+  state => state.filters
+);
+
+export const publicDataGeojson = createSelector(
+  publicDataRequest,
+  req => req.data
+);
+
+// Filter Options (based off of the complete public dataset)
+
+export const allOffices = createSelector(
+  publicDataGeojson,
+  json => {
+    if (!json || !json.features) return [];
+
+    const offices = new Set();
+    json.features.forEach(f => {
+      offices.add(f.properties.officeSought);
+    });
+    return Array.from(offices);
+  }
+);
+
+export const allCampaigns = createSelector(
+  publicDataGeojson,
+  json => {
+    if (!json || !json.features) return [];
+
+    const campaignHash = json.features.reduce((campaigns, f) => {
+      campaigns[f.properties.campaignId] = f.properties.campaignName;
+      return campaigns;
+    }, {});
+
+    return Object.keys(campaignHash).map(id => ({
+      id,
+      name: campaignHash[id],
+    }));
+  }
+);
+
+// Filter selections
+
+export const selectedOffices = createSelector(
+  publicDataFilters,
+  filters => filters.offices || []
+);
+
+export const selectedCampaigns = createSelector(
+  publicDataFilters,
+  filters => (filters.campaigns || []).map(c => c.id)
+);
+
+// Filtered public dataset (based on above filters)
+
+export const filteredPublicData = createSelector(
+  publicDataGeojson,
+  selectedOffices,
+  selectedCampaigns,
+  (data, offices, campaigns) => {
+    // Create a shallow copy of the underlying Geojson
+    const dataCopy = Object.extend({}, data);
+
+    // If selected filter arrays are empty, do not filter
+    if (!offices.length && !campaigns.length) {
+      dataCopy.features = dataCopy.features.slice();
+      return dataCopy;
+    }
+
+    if (offices.length) {
+      dataCopy.features = dataCopy.features.filter(f =>
+        offices.includes(f.properties.officeSought)
+      );
+    }
+
+    if (campaigns.length) {
+      dataCopy.features = dataCopy.features.filter(f =>
+        campaigns.includes(f.properties.campaignId)
+      );
+    }
+
+    return dataCopy;
+  }
+);
+
+// Aggregated data per chart
