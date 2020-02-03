29b916c8200413362626e911575b7cbee327e048
Jaron Heard <jaronheard@gmail.com>
2020-02-03 22:16:03 +0900

    Add map legend (#1032)
    * Add map legend and adjust map visualization colors
    
    add d3-scale dep
    
    factor out css per review comments
    
    * adjust css
    
    * adjust css for mobile

--- app/src/Pages/Home/Home.js
+++ app/src/Pages/Home/Home.js
@@ -16,7 +16,10 @@ import {
   ScreenGridMap,
   BaseMap,
   MapTooltip,
+  MapLegend,
+  VisualizationColors,
 } from '@hackoregon/component-library';
+import { scaleQuantize } from 'd3-scale';
 import PageHoc from '../../components/PageHoc/PageHoc';
 import Table from '../../components/Table';
 import ContributionTypePie from '../../components/Visualizations/ContributionTypePie';
@@ -48,10 +51,12 @@ import ContributorLocationBar from '../../components/Visualizations/ContributorL
 import PublicDateRangeField from '../../components/Fields/PublicDateRangeField';
 
 const { dollars } = civicFormat;
+const scatterplotColor = { rgba: [35, 85, 44, 255], hex: '#23552c' };
+const screenGridColorRange = VisualizationColors.sequential.ocean;
 
 const formStyles = css`
   justify-content: flex-start;
-  margin-left: 16px;
+  margin-left: 1rem;
 
   h1 {
     font-size: 1.5em;
@@ -88,6 +93,53 @@ const formOption = css`
   display: flex !important;
   justify-content: flex-start !important;
   padding: 10px !important;
+`;
+
+const visualizationContainer = css`
+  margin: 2rem 0;
+  display: flex;
+  flex-direction: column;
+  @media ${mediaQueryRanges.mediumAndUp} {
+    flex-direction: row;
+    height: 700px;
+  }
+`;
+
+const legendContainer = css`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 0.5rem;
+`;
+
+const legendStyle = css`
+  font-family: 'Roboto Condensed', 'Helvetica Neue', Helvetica, sans-serif;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const legendMargin = css`
+  margin: 10px 0 0 0;
+`;
+
+const chartContainer = css`
+  @media ${mediaQueryRanges.mediumAndUp} {
+    height: 700px;
+  }
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  margin: 2.5em 0.5em 0em 0.5em;
+`;
+
+const scatterplotFill = css`
+  fill: ${scatterplotColor.hex};
+`;
+
+const mapHeight = css`
+  height: 700px;
+  width: 100%;
 `;
 
 class HomePage extends React.Component {
@@ -215,7 +267,10 @@ class HomePage extends React.Component {
     const sanitize = features => features.filter(f => f.geometry.coordinates);
 
     const getPosition = f => f.geometry.coordinates;
-    const getFillColor = () => [25, 183, 170, 255];
+    const getFillColor = () => scatterplotColor.rgba;
+    const colorScale = scaleQuantize()
+      .domain([0, 1])
+      .range(screenGridColorRange);
 
     return (
       <PageHoc>
@@ -303,86 +358,106 @@ class HomePage extends React.Component {
             components={{ Toolbar: () => <div /> }}
           />
         )}
-        <div
-          css={css`
-            margin: 2rem 0;
-            display: flex;
-            flex-direction: column;
-            @media ${mediaQueryRanges.mediumAndUp} {
-              flex-direction: row;
-              height: 700px;
-            }
-          `}
-        >
+        <div css={visualizationContainer}>
           {!!mapData.features.length && (
             <>
-              <BaseMap
-                updateViewport={false}
-                initialZoom={11}
-                useContainerHeight
-                maxZoom={13}
-              >
-                {!this.state.count ? (
-                  <ScatterPlotMap
-                    data={sanitize(mapData.features)}
-                    getPosition={getPosition}
-                    opacity={0.1}
-                    getFillColor={getFillColor}
-                    radiusScale={5}
-                    getRadius={d => Math.sqrt(d.properties.amount)}
-                    autoHighlight
-                  >
-                    <MapTooltip
-                      primaryName="Campaign"
-                      primaryField="campaignName"
-                      secondaryName="Contribution"
-                      secondaryField="amount"
-                    />
-                  </ScatterPlotMap>
-                ) : (
-                  <div />
-                )}
-                {this.state.count ? (
-                  <ScreenGridMap
-                    data={sanitize(mapData.features)}
-                    getPosition={getPosition}
-                    opacity={1}
-                    colorRange={[
-                      [237, 248, 177],
-                      [199, 233, 180],
-                      [127, 205, 187],
-                      [65, 182, 196],
-                      [29, 145, 192],
-                      [34, 94, 168],
-                      [37, 52, 148],
-                      [8, 29, 88],
-                    ]}
-                    cellSizePixels={15}
-                  >
-                    <MapTooltip
-                      tooltipDataArray={[
-                        {
-                          name: `Number of contributions`,
-                          field: 'cellCount',
-                          formatField: civicFormat.numeric,
-                        },
-                      ]}
-                      isScreenGrid
-                      wide
-                    />
-                  </ScreenGridMap>
-                ) : (
-                  <div />
-                )}
-              </BaseMap>
-              <div
-                css={css`
-                  height: 700px;
-                  display: flex;
-                  flex-direction: column;
-                  justify-content: space-around;
-                `}
-              >
+              <div css={mapHeight}>
+                <div css={legendContainer}>
+                  {this.state.count && (
+                    <legend css={legendStyle}>
+                      <MapLegend
+                        colorScale={colorScale}
+                        formatValues={f =>
+                          f === 0 ? `Fewer Contributions` : `More Contributions`
+                        }
+                        label=""
+                        vertical={false}
+                      />
+                    </legend>
+                  )}
+                  {!this.state.count && (
+                    <legend css={[legendStyle, legendMargin]}>
+                      <span
+                        css={css`
+                          margin-left: 5px;
+                        `}
+                      >
+                        Smaller Contributions
+                      </span>
+                      <span
+                        css={css`
+                          margin-left: 5px;
+                        `}
+                      >
+                        <svg viewBox="0 0 50 10" width="50px">
+                          <circle cx="5" cy="5" r="1" css={scatterplotFill} />
+                          <circle cx="15" cy="5" r="2" css={scatterplotFill} />
+                          <circle cx="25" cy="5" r="3" css={scatterplotFill} />
+                          <circle cx="35" cy="5" r="4" css={scatterplotFill} />
+                          <circle cx="45" cy="5" r="5" css={scatterplotFill} />
+                        </svg>
+                        <span
+                          css={css`
+                            margin-left: 5px;
+                          `}
+                        >
+                          Larger Contributions
+                        </span>
+                      </span>
+                    </legend>
+                  )}
+                </div>
+                <BaseMap
+                  updateViewport={false}
+                  initialZoom={11}
+                  useContainerHeight
+                >
+                  {!this.state.count ? (
+                    <ScatterPlotMap
+                      data={sanitize(mapData.features)}
+                      getPosition={getPosition}
+                      opacity={0.1}
+                      getFillColor={getFillColor}
+                      radiusScale={5}
+                      getRadius={d => Math.sqrt(d.properties.amount)}
+                      autoHighlight
+                    >
+                      <MapTooltip
+                        primaryName="Campaign"
+                        primaryField="campaignName"
+                        secondaryName="Contribution"
+                        secondaryField="amount"
+                      />
+                    </ScatterPlotMap>
+                  ) : (
+                    <div />
+                  )}
+                  {this.state.count ? (
+                    <ScreenGridMap
+                      data={sanitize(mapData.features)}
+                      getPosition={getPosition}
+                      opacity={1}
+                      colorRange={screenGridColorRange}
+                      cellSizePixels={15}
+                    >
+                      <MapTooltip
+                        tooltipDataArray={[
+                          {
+                            name: `Number of contributions`,
+                            field: 'cellCount',
+                            formatField: civicFormat.numeric,
+                          },
+                        ]}
+                        isScreenGrid
+                        wide
+                      />
+                    </ScreenGridMap>
+                  ) : (
+                    <div />
+                  )}
+                </BaseMap>
+              </div>
+              <div css={chartContainer}>
                 <ContributionTypeBar
                   data={aggregatedContributorTypes}
                   count={this.state.count}
@@ -403,7 +478,9 @@ class HomePage extends React.Component {
           <>
             <h2
               css={css`
-                margin-left: 16px;
+                margin-left: 1rem;
+                padding-top: 2rem;
+                font-size: 1.5rem;
               `}
             >
               Campaigns
