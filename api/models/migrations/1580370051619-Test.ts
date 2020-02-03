a20446ab4e13a1092cbdbdb4cb7ed8f541c5559f
Andrew <andyerickson2014@gmail.com>
2020-02-02 21:30:35 -0800

    it is always something, right? Here's a down on migration. it will rename stuff and put back the old enum

--- api/models/migrations/1580370051619-Test.ts
+++ api/models/migrations/1580370051619-Test.ts
@@ -1,10 +1,18 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Test1580370051619 implements MigrationInterface {
+
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`ALTER TYPE "contributions_status_enum" RENAME TO "_old_contributions_status_enum"`);
         await queryRunner.query(`CREATE TYPE "contributions_status_enum" AS ENUM('Archived', 'Draft', 'Submitted', 'Processed', 'Awaiting')`);
         await queryRunner.query(`ALTER TABLE contributions ALTER COLUMN "status" TYPE contributions_status_enum USING status::text::contributions_status_enum`);
 
     }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TYPE "contributions_status_enum" RENAME TO "_failed_contributions_status_enum"`);
+        await queryRunner.query(`ALTER TYPE "_old_contributions_status_enum" RENAME TO contributions_status_enum""`);
+        await queryRunner.query(`ALTER TABLE contributions ALTER COLUMN "status" TYPE contributions_status_enum USING status::text::contributions_status_enum`);
+    }
+
 }
