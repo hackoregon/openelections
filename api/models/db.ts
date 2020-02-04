63e41ab9cf8aea4a0da867174d17a1655db9339e
Andrew <andyerickson2014@gmail.com>
2020-02-03 20:15:04 -0800

    remove this migration in favor of manual effort

--- api/models/db.ts
+++ api/models/db.ts
@@ -5,7 +5,8 @@ export default async (): Promise<Connection> => {
     const connection: Connection = await createConnection(ORMConfig);
     // This is safe to do in prod and dev because it tracks what migrations have ran already in the db.
     if (process.env.NODE_ENV === 'production') {
-        await connection.runMigrations();
+        // If migrations are needed, add use this code:
+        // await connection.runMigrations();
     }
     return connection;
 };
