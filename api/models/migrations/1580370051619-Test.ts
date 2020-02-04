56dc20e950f3294ac6914bda0b9f9081eceaaf55
Andrew <andyerickson2014@gmail.com>
2020-02-03 19:27:56 -0800

    psql optomistic magic (#1041)

--- api/models/migrations/1580370051619-Test.ts
+++ api/models/migrations/1580370051619-Test.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class Test1580370051619 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE contributions ALTER COLUMN "status" ALTER TYPE contributions_status_enum ADD VALUE "Awaiting"`);
+        await queryRunner.query(`ALTER TYPE contributions_status_enum ADD VALUE 'Awaiting'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
