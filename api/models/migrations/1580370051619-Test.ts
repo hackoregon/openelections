8962301b8d7854c10167cf6c25903c894c26982c
Andrew <andyerickson2014@gmail.com>
2020-02-03 18:54:06 -0800

    update migration for awaiting status

--- api/models/migrations/1580370051619-Test.ts
+++ api/models/migrations/1580370051619-Test.ts
@@ -3,16 +3,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class Test1580370051619 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TYPE "contributions_status_enum" RENAME TO "_old_contributions_status_enum"`);
-        await queryRunner.query(`CREATE TYPE "contributions_status_enum" AS ENUM('Archived', 'Draft', 'Submitted', 'Processed', 'Awaiting')`);
-        await queryRunner.query(`ALTER TABLE contributions ALTER COLUMN "status" TYPE contributions_status_enum USING status::text::contributions_status_enum`);
-
+        await queryRunner.query(`ALTER TABLE contributions ALTER COLUMN "status" ALTER TYPE contributions_status_enum ADD VALUE "Awaiting"`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TYPE "contributions_status_enum" RENAME TO "_failed_contributions_status_enum"`);
-        await queryRunner.query(`ALTER TYPE "_old_contributions_status_enum" RENAME TO contributions_status_enum""`);
-        await queryRunner.query(`ALTER TABLE contributions ALTER COLUMN "status" TYPE contributions_status_enum USING status::text::contributions_status_enum`);
     }
 
 }
