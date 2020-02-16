3ebe2037b665e8ee0503e6e1c8b2b02306494153
Jaron Heard <jaronheard@gmail.com>
2020-02-16 11:54:26 -0800

    Update home page with additional content

--- app/src/Pages/Home/Home.js
+++ app/src/Pages/Home/Home.js
@@ -10,6 +10,9 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import Select from '@material-ui/core/Select';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Checkbox from '@material-ui/core/Checkbox';
+import ListItemText from '@material-ui/core/ListItemText';
 import { civicFormat } from '@hackoregon/component-library/dist/utils';
 import {
   ScatterPlotMap,
@@ -18,6 +21,8 @@ import {
   MapTooltip,
   MapLegend,
   VisualizationColors,
+  Logo,
+  Collapsable,
 } from '@hackoregon/component-library';
 import { scaleQuantize } from 'd3-scale';
 import PageHoc from '../../components/PageHoc/PageHoc';
@@ -55,8 +60,14 @@ const scatterplotColor = { rgba: [35, 85, 44, 255], hex: '#23552c' };
 const screenGridColorRange = VisualizationColors.sequential.ocean;
 
 const formStyles = css`
+  display: flex;
+  flex-direction: column;
   justify-content: flex-start;
-  margin-left: 1rem;
+  max-width: max-content;
+  margin: 0 auto 1em auto;
+  padding: 0 1em;
+  border: 1px solid rgb(224, 224, 224);
+  background-color: rgba(243, 242, 243, 0.4);
 
   h1 {
     font-size: 1.5em;
@@ -75,12 +86,11 @@ const formStyles = css`
   }
 `;
 
-const footerStyle = css`
-  text-align: center;
+const dataLoadedStyle = css`
   font-style: italic;
-  padding: 15px;
-  font-size: 0.8em;
-  color: #999;
+  font-size: 0.7em;
+  color: rgba(0, 0, 0, 0.54);
+  text-align: center;
 `;
 
 // The !importants are to override the Mui-* styles
@@ -182,54 +192,50 @@ class HomePage extends React.Component {
       {
         field: 'campaignName',
         title: 'Campaign',
-        sorting: false,
       },
       {
         field: 'officeSought',
         title: 'Office',
-        sorting: false,
       },
       {
         field: 'donationsCount',
         title: 'Total Contributions',
-        sorting: false,
+        defaultSort: 'desc',
       },
       {
         field: 'totalAmountContributed',
         title: 'Total Amount',
-        sorting: false,
         type: 'currency',
       },
       {
         field: 'totalAmountMatched',
         title: 'Total Matched',
-        sorting: false,
         type: 'currency',
       },
       {
         title: 'Micro',
-        sorting: false,
         render: bracketField('micro'),
+        sorting: false,
       },
       {
         title: 'Small',
-        sorting: false,
         render: bracketField('small'),
+        sorting: false,
       },
       {
         title: 'Medium',
-        sorting: false,
         render: bracketField('medium'),
+        sorting: false,
       },
       {
         title: 'Large',
-        sorting: false,
         render: bracketField('large'),
+        sorting: false,
       },
       {
         title: 'Mega',
-        sorting: false,
         render: bracketField('mega'),
+        sorting: false,
       },
     ];
 
@@ -275,11 +281,165 @@ class HomePage extends React.Component {
     return (
       <PageHoc>
         {error && <strong>Oh no! {error}</strong>}
+        <section
+          css={css`
+            padding: 12px;
+            max-width: 700px;
+            margin: 12px auto;
+            button {
+              color: #5e63f6;
+            }
+          `}
+        >
+          <p>
+            <a href="https://portlandoregon.gov/oae">
+              Open and Accountable Elections
+            </a>{' '}
+            is the City of Portland's small donor matching program. It is
+            designed to ensure that the City government is accountable to all
+            Portlanders, not just big campaign donors.
+          </p>
+          <p>
+            The program is open to candidates for Mayor, Commissioner, and
+            Auditor. To participate, they must show broad community support and
+            accept contribution limits. The program provides a 6-to-1 match from
+            the City’s Open & Accountable Elections Fund for the first $50 from
+            any Portland donor.
+          </p>
+          <p>
+            The real-time dashboard below shows contribution sources for
+            participating candidates.
+          </p>
+          <Collapsable description="Open and Accountable Elections">
+            <Collapsable.Section hidden>
+              <h2>How it works</h2>
+              <p>
+                <i>
+                  This is a summary. See the{' '}
+                  <a href="https://www.portlandoregon.gov/oae/">
+                    program website
+                  </a>{' '}
+                  for full details.
+                </i>
+              </p>
+              <ul>
+                <li>Candidates may opt into the program.</li>
+                <li>
+                  To qualify, they must show that they have{' '}
+                  <u>broad community support</u> by collecting 250 small
+                  contributions from Portlanders if running for Council or
+                  Auditor or 500 small contributions if running for Mayor.
+                </li>
+                <li>
+                  Participating candidates must agree to contribution limits.
+                  They must{' '}
+                  <u>
+                    agree to collect no more than $250 per donor and only from
+                    humans, not businesses or political committees
+                  </u>
+                  , with two exceptions:
+                  <ul>
+                    <li>
+                      The first exception is that candidates may raise as much
+                      as $5,000 in “seed money” in order to raise their 250 or
+                      500 qualifying contributions. This helps them hire a
+                      treasurer and campaign manager or print materials and
+                      build a website to collect small contributions. This money
+                      can come from a past campaign, themselves as a gift or a
+                      loan, or from donors.
+                    </li>
+                    <li>
+                      The second exception is $20,000 of in-kind contributions.
+                      This enables candidates to get <i>non-monetary</i> support
+                      – like throwing house parties or creating art for their
+                      campaign – from supporters, up to a total campaign limit
+                      of $20,000.
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  As candidates collect small contributions from Portlanders,
+                  the City matches them 6-to-1 on the first $50. So if you give
+                  a candidate $10 a month, the City will match that with $60
+                  each time until you’ve given $50 and the City has provided the
+                  candidate $300 in matching funds for you.
+                </li>
+                <li>
+                  You can get matched for each race, so you could give to
+                  someone running for Mayor and get matched, and to candidates
+                  in each of the Commission races and get matched, and to a
+                  candidate in the Auditor’s race and get matched.
+                </li>
+              </ul>
+              <h2>What it's for</h2>
+              <p>
+                Our government works best when every person is engaged in the
+                elections process and can make a difference in the community.
+                The program fosters an inclusive democratic process where
+                everyone participates and everyone’s contributions matter. A
+                community where people from all walks of life can run for and
+                win office, while avoiding concerns about the influence of large
+                donations in City elections.
+              </p>
+              <p>
+                We’re strongest when our elected city leaders reflect the full
+                range of talent and lived experience that Portland has to offer,
+                and when the community trusts that elected leaders share the
+                community’s values. People from every neighborhood in Portland
+                should have meaningful opportunities to influence who is elected
+                to City offices, and to run effective citywide campaigns.
+              </p>
+
+              <h2>About the software</h2>
+              <p>
+                This dashboard and the software the powers it was built in
+                partnership between the City’s Open and Accountable Elections
+                program and the{' '}
+                <a href="https://civicsoftwarefoundation.org">
+                  Civic Software Foundation
+                </a>
+                .
+              </p>
+
+              <p>
+                The software is{' '}
+                <a href="https://github.com/hackoregon/openelections/">
+                  open-source
+                </a>{' '}
+                which means that anyone can scrutinize the code — which is
+                essential for true transparency and accountability. It also
+                means that other governments interested in visualizing their
+                campaign finance data can use some or all of the open-source
+                code.
+              </p>
+
+              <p>
+                The software was built by an interdisciplinary team of
+                volunteers and contractors working together through{' '}
+                <a href="https://hackoregon.org">Hack Oregon</a>.
+              </p>
+
+              <h2>Coming soon</h2>
+              <ul>
+                <li>Information on non-participating candidates</li>
+                <li>
+                  Further integration with the{' '}
+                  <a href="civicplatform.org">CIVIC Platform</a> to facilitate
+                  public use and sharing of data visualizations
+                </li>
+                <li>
+                  Documentation of the collaborative software development
+                  process
+                </li>
+              </ul>
+            </Collapsable.Section>
+          </Collapsable>
+        </section>
         <FormGroup row css={formStyles}>
           <h1>
             Contributions for
             <FormControl className="form-control">
-              <InputLabel id="filter-offices">
+              <InputLabel id="filter-offices-label">
                 {`${selectedOffices.length ? '' : 'all '}`}offices
               </InputLabel>
               <Select
@@ -288,13 +448,16 @@ class HomePage extends React.Component {
                 value={selectedOffices}
                 onChange={event => setSelectedOffices(event.target.value)}
                 input={<Input />}
+                renderValue={selected => selected.join(', ')}
               >
                 {allOffices.map(name => (
                   <MenuItem key={name} value={name} css={formOption}>
-                    {name}
+                    <Checkbox checked={selectedOffices.indexOf(name) > -1} />
+                    <ListItemText primary={name} />
                   </MenuItem>
                 ))}
               </Select>
+              <FormHelperText>Limit by position</FormHelperText>
             </FormControl>
             ,
             <FormControl className="form-control">
@@ -307,13 +470,20 @@ class HomePage extends React.Component {
                 value={selectedCampaigns}
                 onChange={event => setSelectedCampaigns(event.target.value)}
                 input={<Input />}
+                renderValue={selected =>
+                  selected.map(campaign => campaign.name).join(', ')
+                }
               >
                 {availableCampaigns.map(campaign => (
                   <MenuItem key={campaign.id} value={campaign} css={formOption}>
-                    {campaign.name}
+                    <Checkbox
+                      checked={selectedCampaigns.indexOf(campaign) > -1}
+                    />
+                    <ListItemText primary={campaign.name} />
                   </MenuItem>
                 ))}
               </Select>
+              <FormHelperText>Limit by candidate</FormHelperText>
             </FormControl>
             <FormControl className="form-control">
               <PublicDateRangeField
@@ -322,6 +492,7 @@ class HomePage extends React.Component {
                 to={selectedEndDate}
                 onChange={setDateRange}
               />
+              <FormHelperText>Limit by date range</FormHelperText>
             </FormControl>
             by
             <FormControl className="form-control">
@@ -338,8 +509,16 @@ class HomePage extends React.Component {
                   count
                 </MenuItem>
               </Select>
+              <FormHelperText>View by amount or count</FormHelperText>
             </FormControl>
           </h1>
+          {!isLoading && (
+            <div css={dataLoadedStyle}>
+              Live data from Open and Accountable Elections retrieved on{' '}
+              {format(timeLoaded, 'MMM DD, YYYY [a]t h:mm:ssa')}.
+              Non-participating candidates not shown.
+            </div>
+          )}
         </FormGroup>
         {!!summaryData && (
           <Table
@@ -493,6 +672,7 @@ class HomePage extends React.Component {
                 pageSize: Math.min(campaignsTable.length, 25),
                 showTitle: false,
                 paging: false,
+                sorting: true,
               }}
               data={campaignsTable}
               perPage={Math.min(campaignsTable.length, 25)}
@@ -502,12 +682,36 @@ class HomePage extends React.Component {
             />
           </>
         )}
-        {!!timeLoaded && (
-          <footer css={footerStyle}>
-            Data from Open and Accountable Elections retrieved on{' '}
-            {format(timeLoaded, 'MMM DD, YYYY [a]t h:mm:ssa')}
-          </footer>
-        )}
+        <footer
+          css={css`
+            text-align: center;
+            margin: 2em auto;
+          `}
+        >
+          <div>Built with and for the public by:</div>
+          <div
+            css={css`
+              margin: 1em auto;
+            `}
+          >
+            <a
+              href="https://www.civicsoftwarefoundation.org"
+              css={css`
+                margin: 1em;
+              `}
+            >
+              <Logo type="standardLogo" alt="CIVIC Logo" />
+            </a>
+            <a
+              href="https://www.hackoregon.org"
+              css={css`
+                margin: 1em;
+              `}
+            >
+              <Logo type="hackOregon" alt="Hack Oregon Logo" />
+            </a>
+          </div>
+        </footer>
       </PageHoc>
     );
   }
