e2056fbbe01055140c527098bcd105f5f693e6b8
Andrew <andyerickson2014@gmail.com>
2020-07-13 22:35:02 -0700

    Change worker to return promise

--- api/jobs/processors/sendNightlyEmailProcessor.ts
+++ api/jobs/processors/sendNightlyEmailProcessor.ts
@@ -1,15 +1,8 @@
-import db from '../../models/db';
+import db from '../helpers/db';
 import { sendActivityEmailToCampaignAdminsAsync } from '../../services/emailService';
 import { renderError } from '../helpers/addJobs';
 
-export default (job: { data: any }, done: any): Promise<any> => {
-    return db().then(async () => {
-        await sendActivityEmailToCampaignAdminsAsync(job.data.id);
-    })
-    .then(() => done())
-    .catch(error => {
-        console.error(error);
-        renderError(error);
-        done();
-    });
+export default async (job: { data: any }): Promise<any> => {
+    await db();
+    return await sendActivityEmailToCampaignAdminsAsync(job.data.id);
 };
