16499e435df1905cff8fb91329608e3928d5690a
Andrew <andyerickson2014@gmail.com>
2020-07-14 00:20:04 -0700

    User BugSnag Iconfig Obj

--- api/services/bugsnagService.ts
+++ api/services/bugsnagService.ts
@@ -2,7 +2,10 @@ import bugsnag from '@bugsnag/js';
 
 export const bugsnagClient = (() => {
     if (process.env.NODE_ENV === 'production') {
-        return bugsnag(process.env.BUG_SNAG_API_KEY);
+        return bugsnag({
+            apiKey: process.env.BUG_SNAG_API_KEY,
+            releaseStage: process.env.APP_ENV
+        });
     }
     return;
 })();
