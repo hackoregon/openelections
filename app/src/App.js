1761aea6d39e4f5496d0d56090f2b50d771ec282
Andrew <andyerickson2014@gmail.com>
2020-11-05 07:39:43 -0800

    Update component library

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,8 @@
-// TODO remove: import { ReactComponent as Logo } from '@hackoregon/component-library/assets/civic-logo-animated.svg';
-// To remove the error: pseudo class ":first-child" is potentially unsafe
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import { Global, css } from '@emotion/core'; // eslint-disable-line no-unused-vars
 import { connect } from 'react-redux';
-import logo from './assets/icons/oae-logo-custom.svg';
+import Logo from '@hackoregon/component-library/assets/civic-logo-animated.svg';
 import Routes from './Pages/routes';
 import FlashMessage from './components/FlashMessage/FlashMessage';
 import styles from './assets/styles/global.styles';
@@ -39,7 +37,7 @@ class App extends React.Component {
       <div css={style}>
         <Global styles={styles} />
         <FlashMessage />
-        {isLoading ? <img src={logo} alt="Logo" /> : <Routes />}
+        {isLoading ? <img src={Logo} alt="Logo" /> : <Routes />}
       </div>
     );
   }
