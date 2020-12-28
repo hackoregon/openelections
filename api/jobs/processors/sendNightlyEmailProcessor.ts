f9f09d1f35f64773536bb5c756ab1fccaadae45b
Andrew <andyerickson2014@gmail.com>
2020-12-28 12:35:03 -0800

    Only run jobs in Prod (#1070)

--- api/jobs/processors/sendNightlyEmailProcessor.ts
+++ api/jobs/processors/sendNightlyEmailProcessor.ts
@@ -2,6 +2,11 @@ import db from '../../models/db';
 import { sendActivityEmailToCampaignAdminsAsync } from '../../services/emailService';
 
 export default async (job: { data: any }): Promise<any> => {
-    await db();
-    return await sendActivityEmailToCampaignAdminsAsync(job.data.id);
+    if (process.env.APP_ENV === 'production') {
+        await db();
+        return await sendActivityEmailToCampaignAdminsAsync(job.data.id);
+    } else {
+        console.log('Not sending job in staging.');
+        return;
+    }
 };
